93dc9444c9ff5a0a982fc50bdb38dfdedd997f1d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-16 23:56:57 -0400

    added folder for non interview challenges and added a program I wrote tonight that searches an array for duplicates and returns the first one found

---
+++ RegularChallenges/challenge2.js
@@ -0,0 +1,26 @@
+// create a program similar to contains duplicates that returns the first duplicate element found in the array.
+
+
+var a = [1, 3, 5, 3, 7, 4, 4];
+
+
+
+function doesContain(a) {
+    var check1 = 0;
+    var check = 0;
+    
+    for (var i = 0; i < a.length; i++) {
+        if (a.indexOf(a[i], i + 1) > -1) {
+            check1 = a[i];
+            
+            
+            if (a.indexOf(a[i]) < a.indexOf(check)){
+                check = check1;
+            }
+        }
+    }
+    return check1;
+
+}
+
+console.log(doesContain(a));
