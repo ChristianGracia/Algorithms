23a54a9dd3e0d84c293e6b4a4b905156133a3cfb
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-20 15:53:41 -0400

    challenge init

---
+++ InterviewPrep/firstNotRepeatingChar.js
@@ -0,0 +1,13 @@
+function firstNotRepeatingCharacter(s) {
+    
+    let arr = s.split('');
+    
+    let arrMap = new Map();
+    
+    for (var i = 0; i < s.length;){
+        arrMap.set(s.charAt(i), 'hi');
+
+        
+    }
+
+}
