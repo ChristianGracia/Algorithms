d62ef30b34a7e35a821e97219bb030d50c49ab9b
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-30 19:10:48 -0400

    created an algorithm that calculates the years since an inputted date and returns the years/months in decimal form

---
+++ otherChallenges/dateCalculator.js
@@ -0,0 +1,35 @@
+function dateCheck(month,year) {
+    
+    var date = new Date();
+    var currentMonth = date.getMonth();
+    var currentYear = date.getFullYear();
+    var yearSub = currentYear - year;
+    var monthsTill = 0;
+    
+    for (var i = month; i < 12; i++)
+    {
+        monthsTill++;
+    }
+    
+    var yearsTill = 0;
+    
+    for(var b = 1; b < yearSub; b++)
+    {
+        yearsTill++;
+    }
+
+    var monthsAfter = 1;
+    
+    for(var c = 0; c < currentMonth; c++)
+    {
+        monthsAfter++;
+    }
+    
+    var total = (monthsTill/12) + (yearsTill) + (monthsAfter/12);
+    var totalFixed = total.toFixed(2);
+    
+    console.log("total is " + totalFixed);
+}
+
+dateCheck(6,2014);
+
