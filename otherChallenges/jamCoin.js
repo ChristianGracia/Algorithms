97c8a1a1aec7682e11c8617bb1d2fe01097ab94b
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-02 23:22:38 -0400

    google code jam problem init

---
+++ otherChallenges/jamCoin.js
@@ -0,0 +1,11 @@
+var coin = 1001;
+
+for (var i = 2; i < 11; i++){
+    console.log(parseInt(coin, i));
+    if (parseInt(coin, i) % 2 !=0){
+        
+    }
+    else 
+}
+
+//first nontrivial to not be prime
