18b51cb5bab6d80cb077752d4f76712894c8bb93
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-04 19:03:44 -0400

    format

--- basicChallenges/centuryCalculator.js
+++ basicChallenges/centuryCalculator.js
@@ -1,11 +1,16 @@
 function centuryFromYear(year) {
     if (year < 100) {
-        return 1
+        return 1;
     }
     else {
-        if (Number.isInteger(year / 100)) {
-            return year / 100
+        if (Number.isInteger(year / 100)) 
+        {
+            return year / 100;
         }
-        else { return (Math.floor((year / 100) + 1)) }
+        else 
+        { 
+            return (Math.floor((year / 100) + 1));
+            
+        }
     }
 }
--- basicChallenges/palindromeChecker.js
+++ basicChallenges/palindromeChecker.js
@@ -1,17 +1,19 @@
 function checkPalindrome(inputString) {
-    var emptyString = ""
-    var length = inputString.length - 1
+    var emptyString = "";
+    var length = inputString.length - 1;
 
-    for (var i = length; i > -1; i--) {
-        emptyString += inputString[i]
+    for (var i = length; i > -1; i--) 
+    {
+        emptyString += inputString[i];
     }
 
 
-    if (emptyString == inputString) {
-        return true
+    if (emptyString == inputString) 
+    {
+        return true;
     }
     else {
-        return false
+        return false;
     }
 
 
