b67ba1479beb605a8b983ca3ec633ec446268548
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-04 18:28:55 -0400

    added function that checks if a given string is a palindrome and returns true or false accordingly

---
+++ basicChallenges/palindromeChecker.js
@@ -0,0 +1,19 @@
+function checkPalindrome(inputString) {
+    var emptyString = ""
+    var length = inputString.length - 1
+
+    for (var i = length; i > -1; i--) {
+        emptyString += inputString[i]
+    }
+
+
+    if (emptyString == inputString) {
+        return true
+    }
+    else {
+        return false
+    }
+
+
+
+}
