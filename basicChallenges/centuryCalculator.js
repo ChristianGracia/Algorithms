8909970c4af2d7b031eb6390d30d647fa8524572
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-04 17:57:15 -0400

    added function that when given a year, calculates the century the year falls in

---
+++ basicChallenges/centuryCalculator.js
@@ -0,0 +1,11 @@
+function centuryFromYear(year) {
+    if (year < 100) {
+        return 1
+    }
+    else {
+        if (Number.isInteger(year / 100)) {
+            return year / 100
+        }
+        else { return (Math.floor((year / 100) + 1)) }
+    }
+}
