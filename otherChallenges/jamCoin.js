a0a725296cc892b0b6da6032df6013ca0c84a4d8
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-03 23:47:31 -0400

    added problem notes

--- otherChallenges/jamCoin.js
+++ otherChallenges/jamCoin.js
@@ -1,3 +1,9 @@
+// google code jam challenge problem
+
+// generate coin with a string of 0 and 1 values.
+// must start and end with a one
+// first nontrivial to must not be prime
+
 var coin = 1001;
 
 for (var i = 2; i < 11; i++){
@@ -8,4 +14,3 @@ for (var i = 2; i < 11; i++){
     else 
 }
 
-//first nontrivial to not be prime
