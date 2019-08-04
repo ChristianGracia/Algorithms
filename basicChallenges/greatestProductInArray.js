dae41efa8610ac61a635890c32b1983e83af122f
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-04 18:32:55 -0400

    added program that looks at all adjacent elements in an array and returns the grest product of these adjacent elements multiplied

---
+++ basicChallenges/greatestProductInArray.js
@@ -0,0 +1,13 @@
+function adjacentElementsProduct(inputArray) {
+    var length = inputArray.length
+    var highestProduct = -100
+
+    for (var i = 0; i < length; i++) {
+        if ((inputArray[i] * inputArray[i + 1]) > highestProduct) {
+            highestProduct = inputArray[i] * inputArray[i + 1];
+        }
+
+    }
+    return highestProduct
+
+}
