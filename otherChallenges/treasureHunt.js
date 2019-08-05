16e33a0e0191c495c48aea1c41355ddf4887395c
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-05 19:27:50 -0400

    N S E W NE NW SE working

--- otherChallenges/treasureHunt.js
+++ otherChallenges/treasureHunt.js
@@ -6,20 +6,10 @@
 
 var size = 9;
 var start = [0, 0];
-var instructions = [{heading: "N", steps: 10}, {heading: "E", steps: 22}, {heading: "S", steps: 13}, {heading: "E", steps: 41}, {heading: "N", steps: 33}]
+var instructions = [{ heading: "SE", steps: 1 }]
 
 function treasureHunt(size, start, instructions) {
     var currentPosition = start;
-
-    // var array = new Array(size);
-
-
-    // for (var i = 0; i < size; i++)
-    // {
-    //     array[i] = new Array(size);
-    // }
-
-    // console.log(array);
 
     for (var j = 0; j < instructions.length; j++) {
         console.log(instructions[j].heading);
@@ -27,19 +17,19 @@ function treasureHunt(size, start, instructions) {
         if (instructions[j].heading == "N") {
             currentPosition[1] += instructions[j].steps;
             if (currentPosition[1] > size) {
-                currentPosition[1] = currentPosition[1] % size
+                currentPosition[1] = currentPosition[1] % size;
             }
         }
         if (instructions[j].heading == "S") {
             currentPosition[1] -= instructions[j].steps;
             if (currentPosition[1] < 0) {
-                currentPosition[1] += size
+                currentPosition[1] = size - (currentPosition[1]%size);
             }
         }
         if (instructions[j].heading == "E") {
             currentPosition[0] += instructions[j].steps;
-            if (currentPosition[0] > size){
-                currentPosition[0] = currentPosition[0] % size
+            if (currentPosition[0] > size) {
+                currentPosition[0] = currentPosition[0] % size;
             }
         }
         if (instructions[j].heading == "W") {
@@ -48,9 +38,39 @@ function treasureHunt(size, start, instructions) {
                 currentPosition[0] = currentPosition += size;
             }
         }
+        if (instructions[j].heading == "NW") {
+            currentPosition[0] -= instructions[j].steps;
+            currentPosition[1] += instructions[j].steps;
+            if (currentPosition[1] > size) {
+                currentPosition[1] = currentPosition[1] % size;
+            }
+            if (currentPosition[0] < 0) {
+                currentPosition[0] = currentPosition[0] += size;
+            }
+        }
+        if (instructions[j].heading == "NE") {
+            currentPosition[0] += instructions[j].steps;
+            currentPosition[1] += instructions[j].steps;
+            if (currentPosition[1] > size) {
+                currentPosition[1] = currentPosition[1] % size;
+            }
+            if (currentPosition[0] > size) {
+                currentPosition[0] = currentPosition[0] % size;
+            }
+        }
+        if (instructions[j].heading == "SE") {
+            currentPosition[0] += instructions[j].steps;
+            currentPosition[1] -= instructions[j].steps;
+            if (currentPosition[1] < 0) {
+                 currentPosition[1] = size - (currentPosition[1]%size)
+            }
+            if (currentPosition[0] > size) {
+                currentPosition[0] = currentPosition[0] % size;
+            }
+        }
 
     }
-    console.log(currentPosition)
+    console.log(currentPosition);
 
 }
 
