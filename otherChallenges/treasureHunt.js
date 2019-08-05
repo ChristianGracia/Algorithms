48d67befdcb6dc2c4f12013fa8aa4f78b43fe5c2
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-05 19:10:46 -0400

    did second part of challenge, user can now loop over sides of the treasure map like a globe

--- otherChallenges/treasureHunt.js
+++ otherChallenges/treasureHunt.js
@@ -5,41 +5,53 @@
 // return coordinates of treasure
 
 var size = 9;
-var start = [0,0];
-var instructions = [{heading: "N", steps: 3}, {heading: "E", steps: 2}, {heading: "S", steps: 1}, {heading: "E", steps: 4}, {heading: "N", steps: 3}];
+var start = [0, 0];
+var instructions = [{heading: "N", steps: 10}, {heading: "E", steps: 22}, {heading: "S", steps: 13}, {heading: "E", steps: 41}, {heading: "N", steps: 33}]
 
-function treasureHunt(size, start, instructions){
-    
-    var array = new Array(size);
-    var currentPosition = start
-    
-    for (var i = 0; i < size; i++)
-    {
-        array[i] = new Array(size);
-    }
-    
-    console.log(array);
-    
-    for (var j = 0; j < instructions.length; j++)
-    {
+function treasureHunt(size, start, instructions) {
+    var currentPosition = start;
+
+    // var array = new Array(size);
+
+
+    // for (var i = 0; i < size; i++)
+    // {
+    //     array[i] = new Array(size);
+    // }
+
+    // console.log(array);
+
+    for (var j = 0; j < instructions.length; j++) {
         console.log(instructions[j].heading);
-        
-        if (instructions[j].heading == "N"){
+
+        if (instructions[j].heading == "N") {
             currentPosition[1] += instructions[j].steps;
+            if (currentPosition[1] > size) {
+                currentPosition[1] = currentPosition[1] % size
+            }
         }
-         if (instructions[j].heading == "S"){
+        if (instructions[j].heading == "S") {
             currentPosition[1] -= instructions[j].steps;
+            if (currentPosition[1] < 0) {
+                currentPosition[1] += size
+            }
         }
-          if (instructions[j].heading == "E"){
+        if (instructions[j].heading == "E") {
             currentPosition[0] += instructions[j].steps;
+            if (currentPosition[0] > size){
+                currentPosition[0] = currentPosition[0] % size
+            }
         }
-          if (instructions[j].heading == "W"){
+        if (instructions[j].heading == "W") {
             currentPosition[0] -= instructions[j].steps;
+            if (currentPosition[0] < 0) {
+                currentPosition[0] = currentPosition += size;
+            }
         }
-        
+
     }
     console.log(currentPosition)
-    
+
 }
 
 treasureHunt(size, start, instructions);
