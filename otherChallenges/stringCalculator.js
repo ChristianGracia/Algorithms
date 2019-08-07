d6805ccfb3a70dacc09a1e333fe3e6bc913f0a58
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-06 20:05:39 -0400

    update

--- otherChallenges/stringCalculator.js
+++ otherChallenges/stringCalculator.js
@@ -1,12 +1,32 @@
-var str = "4 + 4";
+var str = "14 + 4";
 
-function stringCalculator(str){
-    
-    var number = eval(str);
-    console.log(number);
+// function stringCalculatorEasy(str){
+
+//     var number = eval(str);
+//     console.log(number);
+// }
+
+function stringCalculator(str) {
+    var length = str.length;
+
+
+    var firstNum = 0
+
+    for (var i = 0; str[i] != " "; i++) {
+        // if (str[i] == " ") {
+        //     firstNum = str.slice(0, i);
+
+        //     var stop = i;
+
+        // }
+        console.log("hi");
+        
+    }
+
 }
-
 stringCalculator(str);
+
+
 
 
 // function that takes a math problem in a string and evaluates it
