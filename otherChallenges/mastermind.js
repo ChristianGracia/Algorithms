ee62a009108cbdf179e6193972e001b48166f382
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-08 19:55:18 -0400

    mastermind code breaking game fully functional

--- otherChallenges/mastermind.js
+++ otherChallenges/mastermind.js
@@ -1,54 +1,55 @@
 const readline = require("readline");
-const guessCounter = 0;
 
-function masterGame(guessCounter) {
-    var code = "12341234";
+function masterGame() {
+  var code = "12341234";
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    rl.question("What is your guess? ", guess => {
-        console.log(`Guess: ${guess}`);
-        rl.close();
-        masterMind(guess, code);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question("What is your guess? ", guess => {
+    console.log(`Guess: ${guess}`);
+    rl.close();
 
-        if (masterMind(guess, code) == 8) {
-            console.log("you win!");
-        }
-        else {
-            console.log("guess count is: " + guessCounter);
-            masterGame(guessCounter);
-        }
-    });
+    if (masterMind(guess, code) == 8) {
+      console.log("you win!");
+    }
+    if (guessCounter == 8) {
+      console.log("you lose");
+    } else {
+      masterGame();
+    }
+  });
 }
-masterGame(guessCounter);
+
+masterGame();
+var guessCounter = 0;
 
 function masterMind(guess, code) {
-    var length = code.length;
-    var correctPositionCounter = 0;
-    var correctLetterCounter = 0;
+  var length = code.length;
+  var correctPositionCounter = 0;
+  var correctLetterCounter = 0;
 
-    var codeArr = code.split("");
+  var codeArr = code.split("");
 
-    for (var i = 0; i < length; i++) {
-        if (code[i] == guess[i]) {
-            correctPositionCounter++;
-        }
+  for (var i = 0; i < length; i++) {
+    if (code[i] == guess[i]) {
+      correctPositionCounter++;
     }
-    console.log("correct letters in position: " + correctPositionCounter);
+  }
+  console.log("correct letters in position: " + correctPositionCounter);
 
-    for (var j = 0; j < length; j++) {
-        if (codeArr.indexOf(guess[j]) > -1) {
-            correctLetterCounter++;
-            codeArr[j] = " ";
-        }
+  for (var j = 0; j < length; j++) {
+    if (codeArr.indexOf(guess[j]) > -1) {
+      correctLetterCounter++;
+      codeArr[codeArr.indexOf(guess[j])] = " ";
     }
+  }
+  guessCounter++;
 
-    console.log("correct letters = " + correctLetterCounter);
-    console.log([correctPositionCounter, correctLetterCounter]);
+  console.log("correct letters = " + correctLetterCounter);
+  console.log([correctPositionCounter, correctLetterCounter]);
+  console.log("guess: " + guessCounter);
 
-    return correctPositionCounter;
+  return correctPositionCounter;
 }
-
-// masterMind(guess, code);
