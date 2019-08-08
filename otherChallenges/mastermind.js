b667f0c852ac58b4d6383069a6f643f23d16d0da
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-08 19:41:33 -0400

    game almost finished with readline working for user input

--- otherChallenges/mastermind.js
+++ otherChallenges/mastermind.js
@@ -1,8 +1,30 @@
-var guess = "dogs";
-var code = "ssll";
+const readline = require("readline");
+const guessCounter = 0;
 
-function mastermind(guess, code) {
-    
+function masterGame(guessCounter) {
+    var code = "12341234";
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question("What is your guess? ", guess => {
+        console.log(`Guess: ${guess}`);
+        rl.close();
+        masterMind(guess, code);
+
+        if (masterMind(guess, code) == 8) {
+            console.log("you win!");
+        }
+        else {
+            console.log("guess count is: " + guessCounter);
+            masterGame(guessCounter);
+        }
+    });
+}
+masterGame(guessCounter);
+
+function masterMind(guess, code) {
     var length = code.length;
     var correctPositionCounter = 0;
     var correctLetterCounter = 0;
@@ -17,16 +39,16 @@ function mastermind(guess, code) {
     console.log("correct letters in position: " + correctPositionCounter);
 
     for (var j = 0; j < length; j++) {
-        if (codeArr.indexOf(guess[j]) > -1){
+        if (codeArr.indexOf(guess[j]) > -1) {
             correctLetterCounter++;
             codeArr[j] = " ";
-
         }
-        console.log(codeArr)
     }
 
     console.log("correct letters = " + correctLetterCounter);
     console.log([correctPositionCounter, correctLetterCounter]);
+
+    return correctPositionCounter;
 }
 
-mastermind(guess, code);
+// masterMind(guess, code);
