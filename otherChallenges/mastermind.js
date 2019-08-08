a8665e6f13ba7d50ceb467833ace3cf7406088cf
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-08 19:15:29 -0400

    1.0 1.1 working

--- otherChallenges/mastermind.js
+++ otherChallenges/mastermind.js
@@ -1,25 +1,32 @@
-var guess = "john"
-var code = "joan"
+var guess = "dogs";
+var code = "ssll";
 
-function mastermind(guess,code){
-    var length = code.length
+function mastermind(guess, code) {
+    
+    var length = code.length;
     var correctPositionCounter = 0;
-    var correctLetterCounter = length;
-    
-    
-    for (var i = 0; i < length; i++){
-        if(code[i] == guess[i]){
+    var correctLetterCounter = 0;
+
+    var codeArr = code.split("");
+
+    for (var i = 0; i < length; i++) {
+        if (code[i] == guess[i]) {
             correctPositionCounter++;
         }
     }
-    console.log(correctPositionCounter);
-    
-    for (var j = 0; j < length; j++){
-        if (code.indexOf(guess[i]) < 0){
-            correctLetterCounter--;
+    console.log("correct letters in position: " + correctPositionCounter);
+
+    for (var j = 0; j < length; j++) {
+        if (codeArr.indexOf(guess[j]) > -1){
+            correctLetterCounter++;
+            codeArr[j] = " ";
+
         }
+        console.log(codeArr)
     }
+
     console.log("correct letters = " + correctLetterCounter);
+    console.log([correctPositionCounter, correctLetterCounter]);
 }
 
-mastermind(guess,code);
+mastermind(guess, code);
