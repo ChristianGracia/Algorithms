3e740c39dfd3660b740c79a1cf668de927bc3bf8
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-11 03:02:30 -0400

    further attempt for all test cases

--- basicChallenges/almostIncreasingSequence.js
+++ basicChallenges/almostIncreasingSequence.js
@@ -25,3 +25,42 @@ function almostIncreasingSequence(sequence) {
 }
 
 //program checks if the inputted array can be made to be decreasing to increasing values with the removal of only one element.
+
+//alt attempt
+
+function almostIncreasingSequence2(sequence) {
+    var counter = 0
+    var length = sequence.length
+    const flag = false
+    for (var i = 0; i < length - 1; i++)
+    {
+        if (sequence[i] == sequence[i+1]){
+            
+        counter++
+        }
+        if (sequence[i] > sequence[i+1])
+        {
+            counter++
+            console.log(sequence.splice(i,1))
+           if (counter == 1)
+           {
+               sequence.slice(i,1)
+                if (sequence[i] > sequence[i+1])
+                {
+                    counter++
+             
+                }
+            }
+            
+        }
+    }
+    if (counter < 2)
+    {
+        return true;
+    }
+    else {
+        return false
+    }
+    
+    
+}
