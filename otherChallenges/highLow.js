d8caf53adccb71d6183bf20b7f45f6a50dd62f34
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-12 19:09:37 -0400

    fully functional

--- otherChallenges/highLow.js
+++ otherChallenges/highLow.js
@@ -1,44 +1,57 @@
 const readline = require("readline");
+var check1 = false;
+
+var curr = Math.floor(Math.random() * 13) + 2;
 
 function highLowGame() {
-  var curr = Math.floor(Math.random() * 13) + 2;
+  if (check1 == true) {
+    return 0;
+  }
+  var next = Math.floor(Math.random() * 13) + 2;
+
   console.log("current card is: " + curr);
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
   rl.question("high or low? \n", guess => {
-    console.log(`Guess: ${guess}`);
     rl.close();
-    if (highLow(curr, guess) == 1) {
-      console.log("you win");
+    console.log("next card is: " + next);
+
+    if (highLow(curr, guess, next) == 1) {
+      curr = next;
       highLowGame();
-    } else if (highLow(curr, guess) == 0) {
-      console.log("you lose");
+    } else if (highLow(curr, guess, next) == 0) {
       return 0;
     }
   });
 }
 
-function highLow(curr, guess) {
-  var next = Math.floor(Math.random() * 13) + 2;
-  console.log("Next card is: ", next);
+function highLow(curr, guess, next) {
+  var check = false;
 
   if (curr > next && guess == "low") {
     console.log("you win, low is right");
-    return 1;
+    check = true;
+    return check;
   } else if (curr < next && guess == "high") {
     console.log("you win, high is right");
-    return 1;
+    check = true;
+    return check;
   } else if (curr == next) {
+    check = true;
     console.log("pass");
-    return 1;
-  } else if (curr < next && guess == "low") {
+    return check;
+  } else if (check1 == false && curr < next && guess == "low") {
     console.log("you lose, low is wrong");
+    check1 = true;
     return 0;
-  } else if (curr > next && guess == "high") {
+  } else if (check1 == false && curr > next && guess == "high") {
     console.log("you lose, high is wrong");
+    check1 = true;
     return 0;
+  } else {
+    return check;
   }
 }
 
