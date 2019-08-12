99816d8d421658ec751a177acc19efa535303d50
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-12 18:20:28 -0400

    game almost set up

--- otherChallenges/highLow.js
+++ otherChallenges/highLow.js
@@ -1,10 +1,24 @@
 const readline = require("readline");
 
-var curr = 8;
-var guess = "low";
+function highLowGame() {
+  var curr = Math.floor(Math.random() * 13) + 2;
+  console.log("current card is: " + curr);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question("high or low? \n", guess => {
+    console.log(`Guess: ${guess}`);
+    rl.close();
+    highLow(curr, guess);
+  });
+
+  highLowGame();
+}
 
 function highLow(curr, guess) {
-  var next = 7;
+  var next = Math.floor(Math.random() * 13) + 2;
+  console.log("Next card is: ", next);
 
   if (curr > next && guess == "low") {
     console.log("you win, low is right");
@@ -17,23 +31,12 @@ function highLow(curr, guess) {
   }
   if (curr < next && guess == "low") {
     console.log("you lose, low is wrong");
+    return 0;
   }
   if (curr > next && guess == "high") {
     console.log("you lose, high is wrong");
+    return 0;
   }
 }
 
-highLow(curr, guess);
-
-// function highLowGame(curr) {
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   });
-//   rl.question("high or low? \n", guess => {
-//     console.log(`Guess: ${guess}`);
-//     rl.close();
-//   });
-// }
-
-// highLowGame(curr);
+highLowGame();
