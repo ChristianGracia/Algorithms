5f959187dde07ef87f37490da83a2d9c811def4d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-12 19:24:45 -0400

    converted to object

--- otherChallenges/highLow.js
+++ otherChallenges/highLow.js
@@ -1,52 +1,62 @@
 const readline = require("readline");
 var check1 = false;
 
-var curr = Math.floor(Math.random() * 13) + 2;
+var suits = ["clubs", "hearts", "spades", "diamonds"];
+
+var current = {
+  suit: suits[Math.floor(Math.random() * 4)],
+  value: Math.floor(Math.random() * 13) + 2
+};
+
+console.log(current);
 
 function highLowGame() {
   if (check1 == true) {
     return 0;
   }
-  var next = Math.floor(Math.random() * 13) + 2;
+  next = {
+    value: Math.floor(Math.random() * 13) + 2,
+    suit: suits[Math.floor(Math.random() * 4)]
+  };
 
-  console.log("current card is: " + curr);
+  console.log("\n\ncurrent card is: " + current.value + " of " + current.suit);
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
   rl.question("high or low? \n", guess => {
     rl.close();
-    console.log("next card is: " + next);
+    console.log("\n\nnext card is: " + next.value + " of " + next.suit);
 
-    if (highLow(curr, guess, next) == 1) {
-      curr = next;
+    if (highLow(current, guess, next) == 1) {
+      current = next;
       highLowGame();
-    } else if (highLow(curr, guess, next) == 0) {
+    } else if (highLow(current, guess, next) == 0) {
       return 0;
     }
   });
 }
 
-function highLow(curr, guess, next) {
+function highLow(current, guess, next) {
   var check = false;
 
-  if (curr > next && guess == "low") {
+  if (current.value > next.value && guess == "low") {
     console.log("you win, low is right");
     check = true;
     return check;
-  } else if (curr < next && guess == "high") {
+  } else if (current.value < next.value && guess == "high") {
     console.log("you win, high is right");
     check = true;
     return check;
-  } else if (curr == next) {
+  } else if (current.value == next.value) {
     check = true;
     console.log("pass");
     return check;
-  } else if (check1 == false && curr < next && guess == "low") {
+  } else if (check1 == false && current.value < next.value && guess == "low") {
     console.log("you lose, low is wrong");
     check1 = true;
     return 0;
-  } else if (check1 == false && curr > next && guess == "high") {
+  } else if (check1 == false && current.value > next.value && guess == "high") {
     console.log("you lose, high is wrong");
     check1 = true;
     return 0;
