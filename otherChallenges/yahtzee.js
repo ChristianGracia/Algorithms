5263067de18cb0a9e57a08c91cf406ca1d59570e
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-19 18:58:31 -0400

    yahtzee fully functional

--- otherChallenges/yahtzee.js
+++ otherChallenges/yahtzee.js
@@ -5,20 +5,24 @@ var rollArray = fiveRolls();
 
 function yahtzee() {
   rollCounter++;
+
+  if (rollCounter == 4) {
+    console.log("done");
+    return 0;
+  }
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
   rl.question(
-    `dice: ${rollArray}\n Input the number of dice to reroll (1-5) starting from lowest to highest `,
+    `dice: ${rollArray}\n Input the index of dice to reroll (1-5) `,
     answer => {
       console.log(`Answer: ${answer}`);
       rl.close();
 
-      if (rollCounter < 3) {
+      if (rollCounter < 4) {
+        rollArray = dieHandler(answer);
         yahtzee();
-      } else {
-        console.log("Done");
       }
     }
   );
@@ -37,12 +41,28 @@ function fiveRolls() {
   return rollArray;
 }
 
-var rollObject = {
-  roll1: singleRoll(),
-  roll2: singleRoll(),
-  roll3: singleRoll(),
-  roll4: singleRoll(),
-  roll5: singleRoll()
-};
+function dieHandler(answer) {
+  var answerArray = answer.split("");
+  if (answerArray.indexOf("1") > -1) {
+    rollArray[0] = singleRoll();
+    console.log("1 rolled");
+  }
+  if (answerArray.indexOf("2") > -1) {
+    rollArray[1] = singleRoll();
+    console.log("2 rolled");
+  }
+  if (answerArray.indexOf("3") > -1) {
+    rollArray[2] = singleRoll();
+    console.log("3 rolled");
+  }
+  if (answerArray.indexOf("4") > -1) {
+    rollArray[3] = singleRoll();
+    console.log("4 rolled");
+  }
+  if (answerArray.indexOf("5") > -1) {
+    rollArray[4] = singleRoll();
+    console.log("5 rolled");
+  }
 
-function dieHandler() {}
+  return rollArray;
+}
