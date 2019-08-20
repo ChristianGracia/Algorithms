a305dfe9f30bbd180e0fe03bb9f2f6de2d9f6373
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-20 19:12:08 -0400

    dnd program functional

--- otherChallenges/dice20Sided.js
+++ otherChallenges/dice20Sided.js
@@ -1,16 +1,34 @@
+var readlineSync = require("readline-sync");
+
 function singleRoll() {
   var roll = Math.floor(Math.random() * 20) + 1;
   return roll;
 }
 
+// function weaponDamage(x, y) {
+//   var dice = x(Math.floor(Math.random(y)) + 1);
+//   return dice;
+// }
+
 function dnd() {
+  var modifier = readlineSync.question("Whats the modifier \n");
+  var AC = readlineSync.question("Whats the armor class? \n");
+
   roll = singleRoll();
+  console.log("roll was " + roll);
+
   if (roll == 1) {
-    console.log("nice natural 1");
+    console.log("\nnice natural 1, you miss!!!");
   } else if (roll == 20) {
-    console.log("nice natural 20");
+    console.log("\nnice natural 20, you hit!!!");
   } else {
-    console.log(roll);
+    var result = parseInt(roll) + parseInt(modifier);
+    console.log(result);
+    if (parseInt(AC) > result) {
+      console.log("you miss son");
+    } else {
+      console.log("you hit son");
+    }
   }
 }
 
