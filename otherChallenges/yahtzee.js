a2f9426fb530d5cfff502bda69d5b83200e19d53
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-20 00:34:42 -0400

    adding scoring

--- otherChallenges/yahtzee.js
+++ otherChallenges/yahtzee.js
@@ -62,4 +62,98 @@ function dieHandler(answer) {
   return rollArray;
 }
 
-function scorer(counter, rollArray) {}
+function scorer(rollArray, counter) {
+  const options = [
+    " Three of a kind (1)",
+    " Four of a kind (2)",
+    " Full House (3)",
+    " Small Straight (4)",
+    " Large Straight (5)",
+    " Yahtzee (6)",
+    " Chance (7)"
+  ];
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question(`Press option # (1-7) for score: \n${options}\n`, answer => {
+    rl.close();
+
+    switch (true) {
+      case answer == "1": {
+        console.log("answer =" + options[0]);
+        break;
+      }
+      case answer == "2": {
+        console.log("answer =" + options[1]);
+        break;
+      }
+      case answer == "3": {
+        console.log("answer =" + options[2]);
+        break;
+      }
+      case answer == "4": {
+        console.log("answer =" + options[3]);
+        break;
+      }
+      case answer == "5": {
+        console.log("answer =" + options[4]);
+
+        const set1 = new Set(rollArray);
+
+        console.log(set1.size);
+        if (set1.size == 5) {
+          console.log("yes");
+          if (set1.has("1") && set1.has("6")) {
+            console.log("incorrect");
+          } else {
+            console.log("score: 50" + options[4]);
+          }
+        } else {
+          console.log("incorrect");
+        }
+        break;
+      }
+      case answer == "6": {
+        console.log("answer =" + options[5]);
+        console.log("Yahtzee!!!!! score: 50");
+        playAgain();
+        break;
+      }
+      case answer == "7": {
+        console.log("answer =" + options[6]);
+        console.log("score: " + counter);
+        playAgain();
+
+        break;
+      }
+      default:
+        console.log("\nincorrect choice");
+    }
+  });
+}
+
+function playAgain() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question("play again? yes or no\n", answer => {
+    console.log(`Answer: ${answer}\n`);
+    rl.close();
+
+    switch (true) {
+      case answer == "yes": {
+        yahtzee();
+        break;
+      }
+      case answer == "no": {
+        console.log("goodbye");
+        break;
+      }
+      default:
+        console.log("goodbye");
+    }
+  });
+}
