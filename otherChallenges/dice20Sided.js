f62a1d81f8cc1adfef739aeccfa835ea6126311a
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-20 19:40:44 -0400

    fully functional

--- otherChallenges/dice20Sided.js
+++ otherChallenges/dice20Sided.js
@@ -5,15 +5,13 @@ function singleRoll() {
   return roll;
 }
 
-// function weaponDamage(x, y) {
-//   var dice = x(Math.floor(Math.random(y)) + 1);
-//   return dice;
-// }
+function weaponDamage(x, y) {
+  var dice = x * (Math.floor(Math.random() * y) + 1);
+
+  return dice;
+}
 
 function dnd() {
-  var modifier = readlineSync.question("Whats the modifier \n");
-  var AC = readlineSync.question("Whats the armor class? \n");
-
   roll = singleRoll();
   console.log("roll was " + roll);
 
@@ -22,14 +20,34 @@ function dnd() {
   } else if (roll == 20) {
     console.log("\nnice natural 20, you hit!!!");
   } else {
+    var AC = readlineSync.question("Whats the armor class? \n");
+    var modifier = readlineSync.question("Whats the modifier \n");
     var result = parseInt(roll) + parseInt(modifier);
     console.log(result);
-    if (parseInt(AC) > result) {
+    if (parseInt(AC) >= result) {
       console.log("you miss son");
     } else {
+      var answer = readlineSync.question(
+        "what kind of dice for dmg? (3d5, 2d6, etc)\n"
+      );
+
+      var check = stringParse(answer);
+      var x = parseInt(check[0]);
+      var y = parseInt(check[1]);
+
+      var damage = weaponDamage(x, y);
+
       console.log("you hit son");
+
+      console.log("you did " + damage + " damage");
     }
   }
 }
 
 dnd();
+
+function stringParse(string) {
+  var arraySplit = string.split("d");
+  console.log(arraySplit);
+  return arraySplit;
+}
--- otherChallenges/test.js
+++ otherChallenges/test.js
@@ -1,23 +1,8 @@
-var roll = ["2", "2", "2", "1", "4"];
-var roll1 = ["1", "2", "3", "4", "6"];
+function weaponDamage(x, y) {
+  var dice = x * (Math.floor(Math.random() * y) + 1);
+  console.log(dice);
 
-function straight(roll) {
-  var object = {};
-  var result = [];
-
-  roll.forEach(function(item) {
-    if (!object[item]) {
-      object[item] = 0;
-    }
-    object[item] += 1;
-  });
-
-  for (var prop in object) {
-    if (object[prop] >= 2) {
-      result.push(prop);
-    }
-  }
-  console.log(parseInt(result[0]) * 3);
+  return dice;
 }
 
-straight(roll);
+weaponDamage(2, 5);
