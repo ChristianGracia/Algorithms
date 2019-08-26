9a2f35372b27960c23ef0ee6ba7bfa29a6c76cbd
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-26 18:53:17 -0400

    shuffle working

--- otherChallenges/deckRacker.js
+++ otherChallenges/deckRacker.js
@@ -1,18 +1,25 @@
 const deckRacer = {
-  map: [],
+  gameMap: [],
   createMap: function() {
     for (let i = 1; i < 8; i++) {
       for (let j = 1; j < 5; j++) {
         let obj = { card: i, suit: j };
-        this.map.push(obj);
+        this.gameMap.push(obj);
       }
     }
   },
   rollDice: function() {
     let roll = Math.floor(Math.random() * 8) + 1;
     return roll;
+  },
+  shuffleDice: function() {
+    this.gameMap.sort(() => Math.random() * 0.5);
   }
 };
 deckRacer.createMap();
-console.log(deckRacer.rollDice());
-console.log(deckRacer.map);
+
+// console.log(deckRacer.rollDice());
+console.log(deckRacer.gameMap);
+
+deckRacer.shuffleDice();
+console.log(deckRacer.gameMap);
