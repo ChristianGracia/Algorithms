417f2dc54cce9604b6eded45e2aca3cea7ad7d9a
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-27 18:42:49 -0400

    dice class working

--- otherChallenges/finishLine.js
+++ otherChallenges/finishLine.js
@@ -1,5 +1,21 @@
 const game = {
   cardArray: [],
+  diceArray: [0],
+
+  roll: function() {
+    let roll = Math.floor(Math.random() * 6) + 1;
+    return roll;
+  },
+
+  diceClass: (DiceRoll = function(roll1, roll2) {
+    (this.dice1 = roll1), (this.dice2 = roll2);
+  }),
+
+  rollDice: function() {
+    let val = new DiceRoll(this.roll(), this.roll());
+    this.diceArray[0] = val;
+    console.log(this.diceArray);
+  },
 
   mapClass: (GameMap = function(tileNumber, card, suit) {
     this.tileNumber = tileNumber;
@@ -17,3 +33,4 @@ const game = {
 };
 
 game.tileMap();
+game.rollDice();
