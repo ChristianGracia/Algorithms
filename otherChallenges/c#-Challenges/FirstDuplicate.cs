dca799c6b3b2dfac1d86148665e318a32860e596
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-07 22:56:42 -0400

    added another attemp at challenge that passes 21/13 checks using a dictionary instead

--- otherChallenges/c#-Challenges/FirstDuplicate.cs
+++ otherChallenges/c#-Challenges/FirstDuplicate.cs
@@ -1,17 +1,37 @@
-int firstDuplicate(int[] a) {
+int firstDuplicate(int[] a)
+{
     Hashtable hash = new Hashtable();
-    
 
-    for(int i = 0; i < a.Length; i++){
+    for (int i = 0; i < a.Length; i++)
+    {
         if (Array.IndexOf(a, a[i]) != Array.LastIndexOf(a, a[i]))
         {
-            
-            
-       if (hash.Contains(a[i]))
-           return a[i];
+            if (hash.Contains(a[i]))
+                return a[i];
         }
-        hash.Add(a[i], Array.LastIndexOf(a, a[i]) - Array.IndexOf(a, a[i]) );
+        hash.Add(a[i], Array.LastIndexOf(a, a[i]) - Array.IndexOf(a, a[i]));
     }
     return -1;
-  
+
+}
+
+//second with dictionary
+
+int firstDuplicate(int[] a)
+{
+    Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (Array.IndexOf(a, a[i]) != Array.LastIndexOf(a, a[i]))
+        {
+            if (dictionary.ContainsKey(a[i]))
+                return a[i];
+            dictionary.Add(a[i], a[i]);
+
+        }
+
+    }
+    return -1;
+
 }
