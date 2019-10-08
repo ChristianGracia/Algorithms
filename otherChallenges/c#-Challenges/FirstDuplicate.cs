0330f675fe9028bc45cc6f9a1abe614bacf41364
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-07 22:59:14 -0400

    format

--- otherChallenges/c#-Challenges/FirstDuplicate.cs
+++ otherChallenges/c#-Challenges/FirstDuplicate.cs
@@ -12,11 +12,9 @@ int firstDuplicate(int[] a)
         hash.Add(a[i], Array.LastIndexOf(a, a[i]) - Array.IndexOf(a, a[i]));
     }
     return -1;
-
 }
 
 //second with dictionary
-
 int firstDuplicate(int[] a)
 {
     Dictionary<int, int> dictionary = new Dictionary<int, int>();
@@ -28,10 +26,7 @@ int firstDuplicate(int[] a)
             if (dictionary.ContainsKey(a[i]))
                 return a[i];
             dictionary.Add(a[i], a[i]);
-
         }
-
     }
     return -1;
-
 }
