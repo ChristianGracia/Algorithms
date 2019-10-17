c886f6f5d979b647472dc9a4b328ea6c8f715925
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-17 19:37:27 -0400

    two sum algorithm done in 12 mins

---
+++ otherChallenges/javascript/2Sums.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function (nums, target) {
+
+    var indexes = [];
+
+    nums.forEach(e => {
+        for (var i = nums.indexOf(e) + 1; i < nums.length; i++) {
+            if (e + nums[i] == target) {
+                indexes[0] = nums.indexOf(e)
+                indexes[1] = i;
+            }
+        }
+    })
+
+    return indexes;
+};
+
+//time = 13mins
+
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = 0; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [j, i];
+            }
+        }
+    }
+};
