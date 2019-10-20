3fe81300b538ee209dde3eb249a7198b15bcb741
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-19 20:12:15 -0400

    deleted other approach

--- otherChallenges/javascript/2Sums.js
+++ otherChallenges/javascript/2Sums.js
@@ -2,15 +2,18 @@ var twoSum = function (nums, target) {
 
     var indexes = [];
 
-    nums.forEach(e => {
-        for (var i = nums.indexOf(e) + 1; i < nums.length; i++) {
-            if (e + nums[i] == target) {
-                indexes[0] = nums.indexOf(e)
-                indexes[1] = i;
-                break;
+    while (indexes.length < 1)
+        nums.forEach(e => {
+            for (var i = nums.indexOf(e) + 1; i < nums.length; i++) {
+                if (e + nums[i] == target) {
+                    indexes[0] = nums.indexOf(e)
+                    indexes[1] = i;
+                    break;
+                }
             }
-        }
-    })
+        })
+
+
     return indexes;
 };
 
@@ -39,21 +42,3 @@ var twoSum = function (nums, target) {
 };
 
 //time = 13mins
-
-
-
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function (nums, target) {
-
-    for (var i = 0; i < nums.length; i++) {
-        for (var j = 0; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                return [j, i];
-            }
-        }
-    }
-};
