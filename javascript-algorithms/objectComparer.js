31296ac49e80ddcefce446f5e17e63c5b1b54a72
Christiangracia <christiangracia.student@careerdevs.com>
2019-10-22 11:45:37 -0400

    passing most checks

--- javascript-algorithms/objectComparer.js
+++ javascript-algorithms/objectComparer.js
@@ -1,17 +1,33 @@
 function whatIsInAName(collection, source) {
-    var arr = [];
+  var arr = [];
 
-    collection.forEach(x => {
-        let keys = Object.keys(x);
-        let values = Object.values(x);
+  collection.forEach(x => {
+    var check = true;
+    for (var j = 0; j < Object.keys(source).length; j++) {
+      if (
+        !x.hasOwnProperty(
+          Object.keys(source)[j] ||
+            x[Object.keys(source)[j]] !== source[Object.keys(source)[j]]
+        )
+      ) {
+        check = false;
+      }
+    }
+    if (check) {
+      arr.push(x);
+    }
+  });
 
-        source.forEach(y => {
-            if (Object.keys(y) == keys && Object.values(y) == values) {
-                arr.push(x);
-            }
-        })
+  console.log(arr);
 
-    })
-
-    return arr;
+  return arr;
 }
+
+whatIsInAName(
+  [
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" }
+  ],
+  { last: "Capulet" }
+);
