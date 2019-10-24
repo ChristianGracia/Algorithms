ceef3d85665ce82fb6e07ab55dee3ad064089eba
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-23 22:08:01 -0400

    removed logs

--- javascript-algorithms/spinalCaseConverter.js
+++ javascript-algorithms/spinalCaseConverter.js
@@ -1,13 +1,4 @@
 function spinalCase(str) {
-    console.log(str.replace(/(.)([A-Z])/g, `$1 $2`).replace(/\s+|_+/g, "-").replace(/--/g, "-").toLowerCase());
+    return str.replace(/(.)([A-Z])/g, `$1 $2`).replace(/\s+|_+/g, "-").replace(/--/g, "-").toLowerCase();
 }
 
-
-spinalCase('This Is Spinal Tap');
-spinalCase("thisIsSpinalTap");
-spinalCase("This Is Spinal Tap");
-spinalCase("The_Andy_Griffith_Show");
-spinalCase("This Is Spinal Tap");
-spinalCase("Teletubbies say Eh-oh");
-spinalCase("This Is Spinal Tap");
-spinalCase("AllThe-small Things");
