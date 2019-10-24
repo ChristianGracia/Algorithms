ce7af89f0b07c38fce8d097190dba48004668f4d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-23 22:01:35 -0400

    passing more checks

--- javascript-algorithms/pigLatin.js
+++ javascript-algorithms/pigLatin.js
@@ -7,9 +7,16 @@ function translatePigLatin(str) {
         case "o":
         case "u":
             str = str + "way";
-            console.log(str);
             return str;
 
+    }
+
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u") {
+            str = str.substr(i) + str.slice(0, i) + "ay";
+            console.log(str);
+            return str;
+        }
     }
 
 
