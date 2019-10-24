c63cc4fead8c41fccb6ed2899e512aa76bb156b7
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-24 19:29:04 -0400

    missing letters algorithm done in 11 mins

---
+++ javascript-algorithms/missingLetters.js
@@ -0,0 +1,13 @@
+function fearNotLetter(str) {
+    var string = "abcdefghijklmnopqrstuvwxyz";
+    var start = string.indexOf(str[0]);
+
+    for (var i = 0; i < str.length; i++) {
+        if (string[start + i] != str[i]) {
+            return string[start + i];
+        }
+    }
+    return undefined;
+}
+
+fearNotLetter("abce");
