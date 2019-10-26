2e253ab4a6092b96552ee4bd6e9983481efd4d9c
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-26 15:02:13 -0400

    figuring out setup

--- javascript-algorithms/fibonacciSum.js
+++ javascript-algorithms/fibonacciSum.js
@@ -1,10 +1,25 @@
 function sumFibs(num) {
     sum = 0;
-    if (num > 0) {
+    if (num < 1) {
         return sum
     }
 
     sum += 2;
+
+    var k = 1;
+    var l = 1;
+
+    var count = 0
+
+    while (count > num) {
+        count = k + l;
+        sum += count;
+
+    }
+
+
+
+
     return num;
 }
 
