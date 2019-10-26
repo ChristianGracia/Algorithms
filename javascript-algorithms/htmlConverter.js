4c4e1adcd2d047ce2ba1de322f954fe396d6be78
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-26 14:51:14 -0400

    added and completed html converted algorithm in 2 minutes

---
+++ javascript-algorithms/htmlConverter.js
@@ -0,0 +1,11 @@
+function convertHTML(str) {
+    var charsHtml = ['&amp;', '&lt;', '&gt;', '&quot;', "&apos;"];
+
+    var regex = [/&/g, /</g, />/g, /"/g, /'/g];
+
+    str = str.replace(regex[0], charsHtml[0]).replace(regex[1], charsHtml[1]).replace(regex[2], charsHtml[2]).replace(regex[3], charsHtml[3]).replace(regex[4], charsHtml[4])
+
+    return str;
+}
+
+convertHTML("Dolce & Gabbana");
