0679e0facdfeeedfed76cc1c443e48a49bc74e9c
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-25 20:53:01 -0400

    added unique values to set

--- javascript-algorithms/uniqueFromArrays.js
+++ javascript-algorithms/uniqueFromArrays.js
@@ -2,8 +2,13 @@ function uniteUnique(arr) {
     var set1 = new Set();
 
     for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            set1.add(arr[i][j];)
+        }
 
     }
+
+
     return arr;
 }
 
