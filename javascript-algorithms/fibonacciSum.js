f76ef9893a41cb54469ecd850e8bf947d581b0e5
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-26 15:22:55 -0400

    passing some testcases

--- javascript-algorithms/fibonacciSum.js
+++ javascript-algorithms/fibonacciSum.js
@@ -1,19 +1,30 @@
 function sumFibs(num) {
-    sum = 0;
+    var sum = 0;
     if (num < 1) {
         return sum
     }
-
-    sum += 2;
+    if (num == 1) {
+        return 2;
+    }
 
     var arr = [1, 1];
 
     for (var i = 1; i < num; i++) {
+        if ((arr[i - 1] + arr[i]) > num) {
+            break;
+        }
         arr.push(arr[i - 1] + arr[i]);
     }
     console.log(arr);
 
-    return num;
+    arr.forEach(x => {
+        if (x % 2 == 0) {
+            sum += x;
+        }
+    })
+
+    console.log(sum);
+    return sum;
 }
 
-sumFibs(4);
+sumFibs(10);
