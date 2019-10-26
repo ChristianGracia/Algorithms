ff44091fc20e78dc751205b45d30ed7f612faa7d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-25 21:00:06 -0400

    closer to completion

--- javascript-algorithms/uniqueFromArrays.js
+++ javascript-algorithms/uniqueFromArrays.js
@@ -1,15 +1,19 @@
 function uniteUnique(arr) {
     var set1 = new Set();
 
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = 0; j < arr[i].length; j++) {
-            set1.add(arr[i][j];)
+    for (var i = 0; i < arguments.length; i++) {
+        for (var j = 0; j < arguments[i].length; j++) {
+            if (!set1.has(arguments[i][j])) {
+                set1.add(arguments[i][j]);
+            }
+
         }
 
     }
+    console.log(set1);
 
 
-    return arr;
+    return set1;
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
