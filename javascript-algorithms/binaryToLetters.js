78989c3b9c3a1ae169b7f8a153d39b387dd01fbe
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-28 19:34:08 -0400

    added time to complete

--- javascript-algorithms/binaryToLetters.js
+++ javascript-algorithms/binaryToLetters.js
@@ -9,13 +9,13 @@ function binaryAgent(str) {
                 sum += Math.pow(2, k);
             }
         }
-
-
-        newArr.push(sum)
-
+        newArr.push(sum);
     }
     var finalArray = newArr.map(x => x = String.fromCharCode(x))
 
     return finalArray.join("");
 }
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+
+//time 32 mins because of time lost trying to use 2 ^ k instead of Math.pow(2, k);
