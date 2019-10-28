9ea349cf5eb2f5dfa7468e4c5a40f990c789a18d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-28 19:32:07 -0400

    binary number to letter algorithm complete

--- javascript-algorithms/binaryToLetters.js
+++ javascript-algorithms/binaryToLetters.js
@@ -1,21 +1,21 @@
 function binaryAgent(str) {
 
+    var newArr = [];
     var arr = str.split(" ");
-
-    arr.forEach(x => {
-        for (var i = 7, k = 1; i => 0; i++) {
-            if (x[i] == 1) {
-                x[i] = 0;
-                x[i] += 2 * k;
+    for (var l = 0; l < arr.length; l++) {
+        let sum = 0;
+        for (var i = 0, k = 7; i < 8; i++ , k--) {
+            if (arr[l][i] == 1) {
+                sum += Math.pow(2, k);
             }
-            k++;
         }
-    })
-
-    console.log(arr);
 
 
-    return str;
+        newArr.push(sum)
+
+    }
+    var finalArray = newArr.map(x => x = String.fromCharCode(x))
+
+    return finalArray.join("");
 }
-
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
