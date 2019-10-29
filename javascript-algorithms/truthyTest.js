0fe7ad997f94ad4396388ba6778797e72fc77f70
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-28 20:05:11 -0400

    truthy object property checker algorithm completed

--- javascript-algorithms/truthyTest.js
+++ javascript-algorithms/truthyTest.js
@@ -1,17 +1,10 @@
 function truthCheck(collection, pre) {
-
     var bool = true;
-    var arr = [];
-    pre.forEach(x => {
-        arr.push(x);
-    })
-    console.log(arr.join(""));
-
     for (var i = 0; i < collection.length; i++) {
-
-
+        if (!collection[i][pre]) {
+            bool = false;
+        }
     }
-
     return bool;
 }
 
