1ab61a61901da91d6a16921670e30f906db5b230
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-29 18:45:10 -0400

    advanced algorithm passing most test cases

--- javascript-algorithms/caesarCypher.js
+++ javascript-algorithms/caesarCypher.js
@@ -2,17 +2,22 @@ function rot13(str) {
     var arr = [];
     for (var i = 0; i < str.length; i++) {
         let letter = str[i].charCodeAt();
-        if (str[i] = str[i].toUpperCase()) {
-            letter = ((letter - 65 + 13)) % 26;
-            console.log(String.fromCharCode(letter))
+        var str1 = " ";
+
+        if (letter > 96 && letter < 123) {
+            letter = ((letter - 96 + 13) % 26) + 96;
             arr.push(String.fromCharCode(letter));
         }
-
+        if (letter > 64 && letter < 90) {
+            letter = ((letter - 65 + 13) % 26) + 65;
+            arr.push(String.fromCharCode(letter));
+        }
+        else {
+            arr.push(String.fromCharCode(letter));
+        }
     }
-
-    console.log(arr.join())
     return arr.join("");
 }
 
-// Change the inputs below to test
 rot13("SERR PBQR PNZC");
+
