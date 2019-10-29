734b6c42c7f6cc1c0cb9f20da93b2da6744d48ef
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-29 19:00:16 -0400

    refactor algorithm to 14 lines

--- javascript-algorithms/caesarCypher.js
+++ javascript-algorithms/caesarCypher.js
@@ -2,25 +2,17 @@ function rot13(str) {
     var arr = [];
     for (var i = 0; i < str.length; i++) {
         let letter = str[i].charCodeAt();
-        console.log(letter)
-
         if (letter > 96 && letter < 123) {
             letter = ((letter - 96 + 13) % 26) + 96;
-            arr.push(String.fromCharCode(letter));
         }
         if (letter > 64 && letter < 91) {
             letter = (((letter - 65) + 13) % 26) + 65;
-            arr.push(String.fromCharCode(letter));
         }
-        else {
-            arr.push(String.fromCharCode(letter));
-        }
+        arr.push(String.fromCharCode(letter));
     }
     return arr.join("");
 }
-
 //time 27 mins
-
 rot13("SERR PBQR PNZC");
 
 
