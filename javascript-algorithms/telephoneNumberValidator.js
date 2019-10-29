71503673f6181744248794ae726041e9fe62dcdc
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-29 19:55:18 -0400

    refactor algorithm to one conditional

--- javascript-algorithms/telephoneNumberValidator.js
+++ javascript-algorithms/telephoneNumberValidator.js
@@ -7,7 +7,6 @@ function telephoneCheck(str) {
     ) {
         return false;
     }
-
     if (strCleaned.length < 10 || strCleaned.length > 12) {
         return false;
     }
@@ -28,3 +27,19 @@ function telephoneCheck(str) {
 }
 //time 28 mins
 telephoneCheck("555-555-5555");
+
+//refactored down to one conditional
+function telephoneCheck(str) {
+
+    var strCleaned = str.replace(/()\)|\(|-|\s/g, "");
+
+    if ((/\(/g.test(str) == true && /\)/g.test(str) == false ||
+        /\)/g.test(str) == true && /\(/g.test(str) == false) || (strCleaned.length < 10 || strCleaned.length > 12) ||
+        ((strCleaned.length == 12 || strCleaned.length == 11) && strCleaned[0] != 1) || (str.split(" ")[0] > 1 && str.split(" ").length > 1) || (str[0] == "-" || str.slice(
+            str.search(/\(/g), str.search(/\)/g)).length > 4
+        )
+    ) {
+        return false;
+    }
+    return true;
+}
