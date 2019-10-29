15ac05b8090300976e00e759a5fe25c92663d17f
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-29 19:40:57 -0400

    phone validator algorithm complete

--- javascript-algorithms/telephoneNumberValidator.js
+++ javascript-algorithms/telephoneNumberValidator.js
@@ -1,20 +1,30 @@
 function telephoneCheck(str) {
 
-    var check = true;
-    var strCleaned = str.replace(/(.)\)|\(|-|\s/g, "");
+    var strCleaned = str.replace(/()\)|\(|-|\s/g, "");
 
-    if (strCleaned.length < 11 || strCleaned.length > 12) {
-        return false;
-    }
-    if ((strCleaned.length == 12 || strCleaned.length == 11) && strCleaned[0] != 1) {
-        return false;
-    }
-    if (str.split(" ")[0] > 1 && str.split(" ").length > 1) {
+    if (/\(/g.test(str) == true && /\)/g.test(str) == false ||
+        /\)/g.test(str) == true && /\(/g.test(str) == false
+    ) {
         return false;
     }
 
-
-    return check;
+    if (strCleaned.length < 10 || strCleaned.length > 12) {
+        return false;
+    }
+    else if ((strCleaned.length == 12 || strCleaned.length == 11) && strCleaned[0] != 1) {
+        return false;
+    }
+    else if (str.split(" ")[0] > 1 && str.split(" ").length > 1) {
+        return false;
+    }
+    else if (str[0] == "-" || str.slice(
+        str.search(/\(/g), str.search(/\)/g)).length > 4
+    ) {
+        return false
+    }
+    else {
+        return true;
+    }
 }
 
 telephoneCheck("555-555-5555");
