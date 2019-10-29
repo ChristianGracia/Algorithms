2dd0eb5550054e84f6e23c945f01dbbc0788d8e3
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-29 19:21:34 -0400

    algorithm almost complete, passing most checks

--- javascript-algorithms/telephoneNumberValidator.js
+++ javascript-algorithms/telephoneNumberValidator.js
@@ -1,8 +1,15 @@
 function telephoneCheck(str) {
 
     var check = true;
+    var strCleaned = str.replace(/(.)\)|\(|-|\s/g, "");
 
-    if (str.replace(/(|)/g, "").length > 12) {
+    if (strCleaned.length < 11 || strCleaned.length > 12) {
+        return false;
+    }
+    if ((strCleaned.length == 12 || strCleaned.length == 11) && strCleaned[0] != 1) {
+        return false;
+    }
+    if (str.split(" ")[0] > 1 && str.split(" ").length > 1) {
         return false;
     }
 
