5b167b2c4a48e5c074836956347c78af1d77e306
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-30 17:05:51 -0400

    added more readable version of algorithm

--- java-algorithms/movieTheaterViewBlockCount.java
+++ java-algorithms/movieTheaterViewBlockCount.java
@@ -1,3 +1,13 @@
 int seatsInTheater(int nCols, int nRows, int col, int row) { 
     return (nCols - col + 1) * (nRows - row);
 }
+
+//alt method more readable
+
+int seatsInTheater(int nCols, int nRows, int col, int row) { 
+    
+    int colDif = nCols - col + 1;
+    int rowDif = nRows - row;
+    int seatsBlocked = rowDif * colDif;
+    return seatsBlocked;
+}
