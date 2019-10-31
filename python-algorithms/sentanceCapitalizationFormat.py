970f514cf183c346917bb95173f2bb8c6b2fef9b
Christiangracia <christiangracia.student@careerdevs.com>
2019-10-31 14:42:32 -0400

    added one liner version of algorithm I made in python

--- python-algorithms/sentanceCapitalizationFormat.py
+++ python-algorithms/sentanceCapitalizationFormat.py
@@ -1,3 +1,8 @@
+#one liner
+def fixMessage(message):
+    return message[0].upper() + message[slice(1, len(message))].lower()
+
+#longer version
 def fixMessage(message):
     newMessage = message[0].upper()
     counter = 0
