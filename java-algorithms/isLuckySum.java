1c83b360fd4b0b6317a3cc9cca6dfd2a8e5a6bae
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-30 23:26:44 -0400

    passing more test cases

--- java-algorithms/isLuckySum.java
+++ java-algorithms/isLuckySum.java
@@ -9,11 +9,39 @@ boolean isLucky(int n) {
     
     }   
     
-    int secondHalf = (int) (n % (Math.pow(10, digitLength/2)));
+    String secondHalf = Integer.toString((int) (n % (Math.pow(10, digitLength/2))));
     
-    int firstHalf = n / (Math.pow(10, digitLength/2));
-        System.out.println(firstHalf);
-          // System.out.println(c);
- 
-    return true;
+    
+    String firstHalf = Integer.toString((int) (n / (Math.pow(10, digitLength/2))));
+    
+              System.out.println(firstHalf);
+           System.out.println(secondHalf);
+    
+    int firstSum = 0;
+    int secondSum = 0;
+    
+    
+    for (int i = 0; i < secondHalf.length(); i++){
+        
+            System.out.println(secondHalf.charAt(i));
+           System.out.println(firstHalf.charAt(i));
+         firstSum += (int) secondHalf.charAt(i);
+        secondSum += (int) firstHalf.charAt(i);
+
+        
+    }
+    
+               System.out.println(firstSum);
+           System.out.println(secondSum);
+    
+
+
+    
+    if (firstSum == secondSum){
+        return true;
+    }
+    return false;
+
+
 }
+
