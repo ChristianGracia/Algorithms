a5d87a368c99e944d97308e531f0cac38bed40f0
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-30 23:47:31 -0400

    islucky number sum algorithm fully functional in java

--- java-algorithms/isLuckySum.java
+++ java-algorithms/isLuckySum.java
@@ -1,47 +1,33 @@
 boolean isLucky(int n) {
-    
-    int digitLength = 1;
-    int c = n;
-    
-    while(c > 10) {
-        digitLength++;
-        c /= 10;
-    
-    }   
-    
-    String secondHalf = Integer.toString((int) (n % (Math.pow(10, digitLength/2))));
-    
-    
-    String firstHalf = Integer.toString((int) (n / (Math.pow(10, digitLength/2))));
-    
-              System.out.println(firstHalf);
-           System.out.println(secondHalf);
-    
-    int firstSum = 0;
-    int secondSum = 0;
-    
-    
-    for (int i = 0; i < secondHalf.length(); i++){
-        
-            System.out.println(secondHalf.charAt(i));
-           System.out.println(firstHalf.charAt(i));
-         firstSum += (int) secondHalf.charAt(i);
-        secondSum += (int) firstHalf.charAt(i);
 
-        
-    }
-    
-               System.out.println(firstSum);
-           System.out.println(secondSum);
-    
+ int digitLength = 1;
+ double c = (double) n;
+
+ while (c > 10) {
+  digitLength++;
+  c /= 10;
+ }
+
+ String secondHalf = Integer.toString((int)(n % (Math.pow(10, digitLength / 2))));
+ String firstHalf = Integer.toString((int)(n / (Math.pow(10, digitLength / 2))));
+
+ int firstSum = 0;
+ int secondSum = 0;
 
 
-    
-    if (firstSum == secondSum){
-        return true;
-    }
-    return false;
+ for (int i = 0; i < secondHalf.length(); i++) {
 
+  secondSum += Character.getNumericValue(secondHalf.charAt(i));
+ }
 
+ for (int j = 0; j < firstHalf.length(); j++) {
+
+  firstSum += Character.getNumericValue(firstHalf.charAt(j));
+ }
+
+ if (firstSum == secondSum) {
+  return true;
+
+ }
+ return false;
 }
-
