78c68d607816642fa46b8b087209bf2090575586
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-31 00:14:08 -0400

    working on this algorithm

---
+++ java-algorithms/heightMover.java
@@ -0,0 +1,22 @@
+int[] sortByHeight(int[] a) {
+    
+    while(true){
+    for(int i = 0; i < a.length; i++){
+        
+    }
+        
+        let check = true;
+    for(int j = 0; j < a.length -1; j++){
+        if (a[j] < a[j + 1]){
+            check = false;
+        }
+    }  
+        
+        if(check){
+            return true;
+        }
+    }
+    return a;
+
+
+}
