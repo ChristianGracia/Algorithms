9f629e27b00c6adbb1a6da8d4fa50fd8f1b11661
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-03 18:20:26 -0500

    passing 19/20 test cases

--- java-algorithms/makeArrayIncreasing.java
+++ java-algorithms/makeArrayIncreasing.java
@@ -3,22 +3,17 @@ int arrayChange(int[] inputArray) {
      
     int counter = 0;
     for(int i = 1; i < inputArray.length; i++){
-    
-        if(a[i] <= a[i - 1] ){
             while(a[i] <= a[i -1]){
                 counter++;
                 a[i] = a[i] +1;
                 
             }
-        }
-        
-        for(int item : a){
-            System.out.println(item);
-        }
-        
     
         
     }
+       for(int item : a){
+            System.out.println(item);
+        }
     return counter;
 
 }
