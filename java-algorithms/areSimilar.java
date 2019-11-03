6a972daae75c7d19106e1b2c7fdf8e40e1e8180b
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-03 13:09:09 -0500

    passing most tests now

--- java-algorithms/areSimilar.java
+++ java-algorithms/areSimilar.java
@@ -1,47 +1,60 @@
 boolean areSimilar(int[] a, int[] b) {
-    Boolean aCheck = true;
-    Boolean bCheck = true;
+    
+       if (Arrays.equals(a, b)){
+        return true;
+    }
+    
+    int counter = 0;
     
     for(int i = 0; i < a.length; i++){
-            for(int j = i+1; j < a.length -1; j++){ 
-                int temp = a[i];
-                
-                a[i] = a[j];
-                a[j] = temp;
-                System.out.println(a);
-                if (a == b){
-                    return true;
-                }
-                else {
-                     temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
-               
-                    
-                }
+        if (a[i] != b[i]){
+            int swapFirst = a[i];
+            counter++;
+            if(counter > 2){
+                return false;
             }
-        
+            
+                for(int j = i + 1; j < a.length; j++){
+                  
+                    if(a[j] != b[j]){
+                        int swapSecond = a[j];
+                        int[] c = a;
+                   
+                        // System.out.println(c[j]);
+                        //          System.out.println(c[i]);
+                        
+                             for(int item : c){
+                            System.out.println(item);
+                        }
+                 
+                        c[i] = swapSecond;
+                        c[j] = swapFirst;
+                        
+                                        System.out.println("\n");
+//                           System.out.println(c[j]);
+//                                  System.out.println(c[i]);
+                        
+                                
+                        for(int item : c){
+                            System.out.println(item);
+                        }
+                        
+                    
+                        
+                        if (Arrays.equals(c, b)){
+                             return true;
+                         }
+                        return false;
+                        
+                              
+                    }
+                }
+    
+            
+        }
     }
     
-      
-    for(int l = 0; l < b.length; l++){
-            for(int m = l+1; m < b.length; m++){ 
-                int temp = b[l];
-                
-                b[l] = b[m];
-                b[m] = temp;
-                if (a == b){
-                    return true;
-                }
-                else {
-                     temp = b[l];
-                    b[l] = b[m];
-                    b[m] = temp;
-               
-                    
-                }
-            }
-        
-    }
+    
     return false;
+
 }
