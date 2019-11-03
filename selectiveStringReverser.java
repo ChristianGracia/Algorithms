e87185c9db60875f331fec9046f3a7f4a09fe645
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-03 01:28:36 -0500

    added alt method I found

--- selectiveStringReverser.java
+++ selectiveStringReverser.java
@@ -48,3 +48,46 @@ String reverseInParentheses(String inputString) {
  return returnString;
 
 }
+
+
+String reverseInParentheses(String s) {
+
+ char[] chars = s.toCharArray();
+    int[] openPosStack = new int[chars.length];
+    int openPosTop = -1;
+
+    for (int i = 0; i < chars.length; i++) {
+        switch (chars[i]) {
+          case '(':
+            // Push the index of the open parenthesis onto the stack
+            openPosStack[++openPosTop] = i;
+            break;
+          case ')':
+            // Reverse substring from the matching open parenthesis to here
+            if (openPosTop < 0) {
+                throw new IllegalArgumentException("Parenthesis mismatch");
+            }
+            int a, b;
+            for (a = openPosStack[openPosTop--], b = i; a < b; a++, b--) {
+                char swap = chars[a];
+                chars[a] = chars[b];
+                chars[b] = swap;
+            }
+        }
+    }
+    if (openPosTop >= 0) {
+        throw new IllegalArgumentException("Parenthesis mismatch");
+    }
+
+    // Remove parentheses from output
+    int o = 0;
+    for (int i = 0; i < chars.length; i++) {
+        switch (chars[i]) {
+          case '(': case ')':
+            break;
+          default:
+            chars[o++] = chars[i];
+        }
+    }
+    return new String(chars, 0, o);
+}
