70c7e3fa5ede0c6c07836973dfe962af04175d8d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-03 18:56:35 -0500

    program I wrote in java that checks if a string can be rearranged to be a palindrome

---
+++ java-algorithms/palindromeReArrange.java
@@ -0,0 +1,35 @@
+boolean palindromeRearranging(String inputString) {
+    
+    int[] array = new int[26];
+    
+    for(int i = 0; i < inputString.length(); i++)
+    {
+        int num = (int)Character.toLowerCase(inputString.charAt(i)) - 97;
+        
+       if(num >= 0 && num < 123){
+           array[num]++;
+            
+         }
+        
+    }
+    
+    Boolean check = true;
+    int counter = 0;
+    
+    for(int item : array){
+     
+        if (item % 2 != 0){
+            counter++;
+            
+        }
+        if(counter > 1){
+            return false;
+        }
+        
+      
+        
+    }
+    
+  
+    return check;
+}
