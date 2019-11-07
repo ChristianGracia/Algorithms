cc8c058dbd7c8bdcaf9fceae95006a2e2b36b9af
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-06 20:15:50 -0500

    will finish later, very close but only had a few mins

--- java-algorithms/longestSubstringInString.java
+++ java-algorithms/longestSubstringInString.java
@@ -2,17 +2,19 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         
         int num = 0;
+        int counter = 0;
         
     
         
         for(int i = 0; i < s.length(); i++){
             
-            int counter = 0;
-            char[] arr = new char[26];
+            int[] arr = new int[26];
             
             int charToNum = (int)Character.toLowerCase(s.charAt(i)) - 97;
+            arr[charToNum]++;
             
-            if(s.charAt(i) != " " || arr[i] < 1 ){
+            
+            if(s.charAt(i) != ' ' && arr[charToNum] <= 1 ){
                 counter++;
             }
             else {
@@ -22,14 +24,10 @@ class Solution {
             if(counter > num){
                 num = counter;
             }
-            
-            return num;
-            
-            
-            
-            
+              
             
         }
+             return num;
         
     }
 }
