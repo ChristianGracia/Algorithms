6a03e993b869453db89e2f8121581db574e8ae41
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-08 15:46:38 -0500

    algorithm redone and passing most checks

--- java-algorithms/ipv4Validator.java
+++ java-algorithms/ipv4Validator.java
@@ -1,25 +1,16 @@
 boolean isIPv4Address(String inputString) {
-    String[] words = inputString.split("\\.");
-       if(words.length != 4){
+    String[] split = inputString.split(".");
+    if (split.length != 4){
         return false;
     }
-    int counter = 0;
-    int[] array = new int[inputString.length()];
-    
-//     for(String item : words){
-//         array[counter] = Integer.parseInt(item);
-//         // Integer num = Integer.parseInt(item);
-   
-//         // if (Integer.parseInt(item) < 0 ||  Integer.parseInt(item) > 255){
-//         //     return false;
-//         // }
-//     }
-    
-    for (String item : words){
-             System.out.println(Integer.parseInt(item));
+    for(String item : split){
+        int num = Integer.parseInt(item);
+        System.out.println(num);
+        if (num < 0 || num > 255){
+            return false;
+        }
     }
-    
- 
     return true;
+    
 
 }
