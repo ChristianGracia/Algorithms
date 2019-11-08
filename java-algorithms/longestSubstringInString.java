336451276b5831522a8a4aaf1537b00b0a0da655
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-08 15:17:21 -0500

    algorithm fully functional

--- java-algorithms/longestSubstringInString.java
+++ java-algorithms/longestSubstringInString.java
@@ -1,60 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         
-        int num = 0;
+        int num = 1;
         int counter = 0;
-        int[] arr = new int[27];
-    
-      
-        for(int i = counter; i < s.length(); i++){
-            
-            int charToNum = (int)Character.toLowerCase(s.charAt(i)) - 97;
-            if(s.charAt(i) == ' '){
-                charToNum = 26;
-                
-            }
-            
         
-            if((charToNum > -1 && charToNum < 27 ) && arr[charToNum] == 0){
-                
-                    arr[charToNum]++;
-    
-                counter++;
-                if(counter > num ){
-                num = counter;
-               }
-            }
-            else {
-                i = s.indexOf(s.charAt(i)) + 1;
-                counter = 1;
-                for(int item : arr){
-                    item = 0;
-                }
-                if(charToNum > -1 && charToNum < 27){
-                         arr[charToNum]++;
-                }
-         
-            }
-  
-            
-        
+        if(s.length() == 0) {
+            return 0;
         }
-             return num;
-    }
         
-    
-}
-
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        
-        int num = 0;
-        int counter = 0;
-        int counter2 = 0;
+     
         while(counter < s.length()){
+            int counter2 = 1;
             String testString = "";
-            for(int i = counter; i < s.length(); i++){
-                testString += s.charAt(i);
+            testString += s.charAt(counter);
+            for(int i = counter + 1; i < s.length(); i++){
+                   testString += s.charAt(i);
+             
                 
                 if(testString.indexOf(s.charAt(i)) == testString.lastIndexOf(s.charAt(i))){
                     counter2++;
@@ -63,8 +24,7 @@ class Solution {
                     }
                 }
                 else {
-                    counter2 = 0;
-                    i = s.length();
+                    break;
                 }
             
                 
