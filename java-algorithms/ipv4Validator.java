26682a8391a5be08fd0e4155ad170b8b05aa21bd
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-17 23:44:20 -0500

    algorithm fully functional in java

--- java-algorithms/ipv4Validator.java
+++ java-algorithms/ipv4Validator.java
@@ -1,32 +1,32 @@
 boolean isIPv4Address(String inputString) {
-
+    
     String[] stringSplit = inputString.split("[.]");
-
-    if (stringSplit.length != 4) {
+    
+    if (stringSplit.length != 4){
         return false;
     }
-
-    for (int i = 0; i < stringSplit.length; i++) {
-
+  
+    for(int i = 0; i < stringSplit.length; i++){
+        
         String number = stringSplit[i];
-
-
-        try {
-            int num = Integer.parseInt(number);
-
-            if (num < 0 || num > 255) {
-                return false;
-            }
-
-            return true;
-        } catch (NumberFormatException e) {
+    
+        
+          try {  
+	        int num = Integer.parseInt(number);  
+              
+            if (num < 0 || num > 255){
             return false;
-        }
-
+            }
+              
+	        
+	      } catch (NumberFormatException e) {  
+	         return false;  
+	      }  
+  
 
     }
     return true;
-
+    
 
 
 }
