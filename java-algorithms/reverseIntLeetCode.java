313689c668944162620f3262c3b59603a43e63ba
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-24 23:56:57 -0500

    algorithm fully functional

--- java-algorithms/reverseIntLeetCode.java
+++ java-algorithms/reverseIntLeetCode.java
@@ -4,20 +4,37 @@ class Solution {
         
         String numberString = Integer.toString(x);
     
-    int length = numberString.length();
+
         String returnString = "";
+        boolean check = false;
+        
+        
+        if(numberString.charAt(0) == '-'){
+            check = true;
+            numberString = numberString.substring(1);
+        }
+        
+        int length = numberString.length();
     
     for(int i = length - 1; i > -1; i--){
-        string += Character.toString(numberString.charAt(i));
-        
+        returnString += Character.toString(numberString.charAt(i));
     }
         
-        System.out.println(string);
-        
-    
-        return Integer.valueOf(string);
-    
+        try
+    {
+        int value = Integer.valueOf(returnString);     
+        if(check)
+        {
+            return (-1 * value );
+        }
 
-        
+        return value;
+    }
+    catch (NumberFormatException nfe)
+    {
+      System.out.println("NumberFormatException: " + nfe.getMessage());
+    }
+        return 0;
+
     }
 }
