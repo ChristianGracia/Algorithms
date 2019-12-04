3888960c0b6015496deea2a4ec1e88a16c6b45b1
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-03 23:59:22 -0500

    storing and reversing linked list values in array list

--- c#-algorithms/LinkedListLeetCode.cs
+++ c#-algorithms/LinkedListLeetCode.cs
@@ -8,28 +8,31 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    
-        String num1 = "";
-        String num2 = "";
+    ArrayList<Integer> num1 = new ArrayList<Integer>();
+    ArrayList<Integer> num2 = new ArrayList<Integer>();
         
         while(l1.next != null){
-            num1 += Integer.toString(l1.val);
+            num1.add(l1.val);
                 l1 = l1.next;
         
                 
         }
         
                while(l2.next != null){
-            num2 += Integer.toString(l2.val);
+            num2.add(l2.val);
                 l2 = l2.next;
         
                 
         }
         
-        num1 += l1.val;
-         num2 += l1.val;
-        System.out.println(num1);
-            System.out.println(num2);
+        num1.add(l1.val);
+         num2.add(l1.val);
+        Collections.reverse(num1);
+            Collections.reverse(num2);
+        
+        int sum = Integer.parseInt(num1) + Integer.parseInt(num2);
+        System.out.println(sum);
+
 
         return l1;
         
