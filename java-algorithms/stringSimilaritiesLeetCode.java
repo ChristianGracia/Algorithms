c47adbdf4af59758b85a020c564947906ec42bd2
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-06 00:25:16 -0500

    algorithm done in java, faster then 94.27% of other submissions on leetcode

--- java-algorithms/stringSimilaritiesLeetCode.java
+++ java-algorithms/stringSimilaritiesLeetCode.java
@@ -1,16 +1,19 @@
-ArrayList<String> chars = new ArrayList<String>();
-        ArrayList<String> matches = new ArrayList<String>();
-        
-        for (String item : J){
-            chars.add(item);
-        }
-        
-        for (String letter : chars){
-            for(String otherListLetter : S){
-                if (leter == otherListLetter && !matches.contains(letter)){
-                    matches.add(letter);
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+
+        int counter = 0;
+
+        for (int i = 0; i < J.length(); i++) {
+            for (int j = 0; j < S.length(); j++) {
+                if (J.charAt(i) == S.charAt(j)) {
+                    counter++;
                 }
+
             }
-          
+
         }
-        return matches.size();
+
+        return counter;
+
+    }
+}
