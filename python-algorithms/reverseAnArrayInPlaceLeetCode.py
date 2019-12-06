d4ac074866a816997c7d844212d23c28714a2964
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-06 16:36:11 -0500

    wrote last algorithm in java, reverse a string in place with no new memory allocation

---
+++ python-algorithms/reverseAnArrayInPlaceLeetCode.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def reverseString(self, s):
+        start = 0
+        end = len(s) -1
+        while start < end:
+            temp = s[start]
+            s[start] = s[end]
+            s[end] = temp
+            start += 1
+            end -= 1
+          
+        
