ab880ae9a99768a76a064a5e84cf49eafeed4987
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-08 16:09:05 -0500

    prettify, fully functional

--- c#-algorithms/findMostRepeatedInArray.cs
+++ c#-algorithms/findMostRepeatedInArray.cs
@@ -35,32 +35,3 @@ public class Solution {
   return maxRepeatedValue;
  }
 }
-
-
-public class Solution {
-    public int MajorityElement(int[] nums) {
-
-        
-        int maxNumber = nums.Max();
-        int maxIndex = 0;
-        int mostRepeatedValue = 0;
-        
-        int[] arr = new int[maxNumber+1];
-        
-        foreach(int item in nums){
-            arr[item]++;
-        }
-
-        
-        for(int i = beginning; i < arr.Length; i++){
-            if (arr[i] > maxIndex){
-                maxIndex = arr[i];
-                mostRepeatedValue = i;
-                
-            }
-               
-        }
-        
-        return mostRepeatedValue;  
-    }
-}
