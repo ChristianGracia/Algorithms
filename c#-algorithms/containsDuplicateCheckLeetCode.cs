65cfd20b081800b1dea499590ae443ec7553caa8
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-07 23:16:06 -0500

    duplicate check algorithm completed in c#

--- c#-algorithms/containsDuplicateCheckLeetCode.cs
+++ c#-algorithms/containsDuplicateCheckLeetCode.cs
@@ -1,5 +1,15 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
+      if (nums.Length != nums.Distinct().Count())
+          return true;
+    return false;
+    }
+    
+}
+
+
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
     foreach(int item in nums){
      if(Array.IndexOf(nums, item) != Array.LastIndexOf(nums, item)){
          return true;
