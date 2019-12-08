34e879b3b62ae8a2e55fbfa12ba64ec638cc0c2c
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-08 01:43:51 -0500

    algorithm redone with ternary in c#

--- c#-algorithms/FizzBuzzLeetCode.cs
+++ c#-algorithms/FizzBuzzLeetCode.cs
@@ -1,16 +1,32 @@
 public class Solution {
+ public IList <string> FizzBuzz(int n) {
+  IList <string> returnString = new List <string>();
+
+  foreach(int num in Enumerable.Range(1, n)) {
+      
+      String value =t num % 5 == 0 &t num % 3 == 0) ? "FizzBuzz" :t num % 3 == 0) ? "Fizz" :t num % 5 == 0) ? "Buzz" : Convert.ToStrint num);
+      
+      returnString.Add(value);
+
+  }
+  return returnString;
+ }
+}
+
+
+public class Solution {
  public IList < string > FizzBuzz(int n) {
   IList < string > returnString = new List < string > ();
 
-  foreach(int index in Enumerable.Range(1, n)) {
-   if (index % 5 == 0 && index % 3 == 0)
+  foreach(int num in Enumerable.Range(1, n)) {
+   ift num % 5 == 0 &t num % 3 == 0)
     returnString.Add("FizzBuzz");
-   else if (index % 3 == 0)
+   else ift num % 3 == 0)
     returnString.Add("Fizz");
-   else if (index % 5 == 0)
+   else ift num % 5 == 0)
     returnString.Add("Buzz");
    else {
-    returnString.Add(Convert.ToString(index));
+    returnString.Add(Convert.ToStrint num));
    }
   }
   return returnString;
