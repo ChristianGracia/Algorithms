511d96c0c5b7be712040465f63d9e7085bd0b373
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-08 00:12:19 -0500

    passing most test cases

--- java-algorithms/maxDepthBinaryTreeLeetCode.java
+++ java-algorithms/maxDepthBinaryTreeLeetCode.java
@@ -4,29 +4,37 @@ class Solution {
 
         int max = 0;
         int counter = root == null ? 0 : 1;
+        if (counter == 0) {
+            return 0;
+        }
 
         TreeNode trav = root;
         TreeNode prev = null;
 
-        trav = trav.left;
-        prev = trav;
-        if (trav.left != null) {
-            trav = trav.left;
-            counter++;
-        } else if (trav.right != null) {
-            trav = trav.right;
-            counter++;
-        } else {
-            counter--;
-            if (prev.right != null) {
-                trav = prev.right;
-
+        while (trav.left != null && trav.right != null) {
+            prev = trav;
+            if (trav.left != null) {
+                trav = trav.left;
+                counter++;
+            } else if (trav.right != null) {
+                trav = trav.right;
+                counter++;
             } else {
-                return counter;
+                counter--;
+                if (prev.right != null) {
+                    trav = prev.right;
+
+                }
+
+                else {
+                    return counter;
+                }
+
             }
 
         }
-        return counter;
+
+        return counter + 1;
     }
 
 }
