0dd022b31e612763d235ff2602d6123ecbda7967
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-08 13:41:35 -0500

    added algorithm solution that passes all checks

--- c#-algorithms/moveArrayValuesInPlaceLeetCode.cs
+++ c#-algorithms/moveArrayValuesInPlaceLeetCode.cs
@@ -1,19 +1,37 @@
 public class Solution {
     public void MoveZeroes(int[] nums) {
-        
-        for(int i = 0; i< nums.Length -1; i++){
-           if(nums[i] == 0){
-               for(int j = i; j < nums.Length -1; j++){
-                   int tmp = nums[j];
-                   nums[j] = nums[j+1];
-                   nums[j+1] = tmp;
-               }
-         
-            }
+         int lastNoneZeroIndex = 0;
+         for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] != 0) 
+                nums[lastNoneZeroIndex++] = nums[i];    
+         }
             
+        for (int i = lastNoneZeroIndex; i < nums.Length; i++) 
+        {
+            nums[i] = 0;
         }
-        
-   
+    
+    }
+}
 
+
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        
+        for(int i = 0; i< nums.Length; i++)
+        {
+        for(int k = 0; k < nums.Length; k++)
+        {
+            if(nums[i] == 0){
+                for(int j = i; j < nums.Length -1; j++){
+           
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                  
+                }
+            }
+        }
+        }
     }
 }
