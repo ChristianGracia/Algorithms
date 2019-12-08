cb2a13da89ea9fe8a3413ae9639cc87432bfd341
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-08 02:25:02 -0500

    reverse linked list algorthim passing most tests in c#

---
+++ c#-algorithms/reverseLinkedListLeetCode.cs
@@ -0,0 +1,26 @@
+
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        
+        ListNode trav = head;
+        List<int> numList = new List<int>();
+        numList.Add(head.val);
+
+        while(trav.next != null){
+            trav = trav.next;
+            numList.Add(trav.val);
+        }
+        numList.Reverse();
+        ListNode returnNode =  new ListNode(numList[0]);
+        trav = returnNode;
+      
+        
+        for(int i = 1; i < numList.Count; i++){   
+            trav.next = new ListNode(numList[i]);
+            trav = trav.next;
+
+        }
+        return returnNode;
+
+    }
+}
