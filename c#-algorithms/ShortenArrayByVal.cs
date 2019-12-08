ad6c2a606ea80316cb9f71f96bff1488324d4077
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-08 14:11:35 -0500

    shorten Array in place algorithm passing all test cases in c#

--- c#-algorithms/ShortenArrayByVal.cs
+++ c#-algorithms/ShortenArrayByVal.cs
@@ -1,23 +1,13 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
         
-        int lastNonRemoved = 0;
-        
+        int valIndex = 0;
         for(int i = 0; i < nums.Length; i++){
             if(nums[i] != val)
             {
-               nums[lastNonRemoved++] = nums[i];
+               nums[valIndex++] = nums[i];
             }
         }
-        
-        for (int newZero = lastNonRemoved; newZero < nums.Length; newZero++) 
-        {
-            nums[newZero] = 0;
-        }
-        int arrLength = nums.Length - lastNonRemoved;
-        Console.WriteLine(arrLength);
-
-        return arrLength;
-
+        return valIndex;
     }
 }
