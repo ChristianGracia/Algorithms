3146125ede2d32e5f58b991e4b2bcd4cda4b645e
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-08 00:30:55 -0500

    binary tree max depth calculator done in java, very fast solution

--- java-algorithms/maxDepthBinaryTreeLeetCode.java
+++ java-algorithms/maxDepthBinaryTreeLeetCode.java
@@ -1,40 +1,14 @@
-
 class Solution {
     public int maxDepth(TreeNode root) {
 
-        int max = 0;
-        int counter = root == null ? 0 : 1;
-        if (counter == 0) {
+        if (root == null) {
             return 0;
+        } else {
+            int leftDepth = maxDepth(root.left);
+            int rightDepth = maxDepth(root.right);
+
+            int num = leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;
+            return num;
         }
-
-        TreeNode trav = root;
-        TreeNode prev = null;
-
-        while (trav.left != null && trav.right != null) {
-            prev = trav;
-            if (trav.left != null) {
-                trav = trav.left;
-                counter++;
-            } else if (trav.right != null) {
-                trav = trav.right;
-                counter++;
-            } else {
-                counter--;
-                if (prev.right != null) {
-                    trav = prev.right;
-
-                }
-
-                else {
-                    return counter;
-                }
-
-            }
-
-        }
-
-        return counter + 1;
     }
-
 }
