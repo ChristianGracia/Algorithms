4427d180b0878d9fcef36bb0c05140037a177225
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-08 16:24:07 -0500

    algorithm in c# almost done, this algorithm searches an array for all values that appear more then 1/3 of the total elements and returns a List

---
+++ c#-algorithms/findFrequentsInArray.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public IList<int> MajorityElement(int[] nums) {
+        List<int> numList = new List<int>();
+        int length = (int)Math.Floor(nums.Length/3d);
+        
+        foreach(int item in nums){
+            int itemFreq = Array.FindAll(nums, e => e == item).Length;
+            if(itemFreq >= length){
+                numList.Add(item);
+            }
+           nums = Array.FindAll(nums, e => e != item);
+            
+        }
+
+        return nums;
+        
+    }
+}
