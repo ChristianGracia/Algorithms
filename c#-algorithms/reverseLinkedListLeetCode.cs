76f7151f87338142b120ee58995b31f7c09cf3c0
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-08 02:26:48 -0500

    prettify final algorithm commit

--- c#-algorithms/reverseLinkedListLeetCode.cs
+++ c#-algorithms/reverseLinkedListLeetCode.cs
@@ -1,30 +1,29 @@
-
 public class Solution {
-    public ListNode ReverseList(ListNode head) {
-        if(head == null){
-            return null;
-        }
-        
-        ListNode trav = head;
-        List<int> numList = new List<int>();
-        numList.Add(head.val);
+ public ListNode ReverseList(ListNode head) {
+  if (head == null) {
+   return null;
+  }
 
-        while(trav.next != null){
-            trav = trav.next;
-            numList.Add(trav.val);
-        }
-        numList.Reverse();
-        ListNode returnNode =  new ListNode(numList[0]);
-        trav = returnNode;
-      
-        
-        for(int i = 1; i < numList.Count; i++){   
-            trav.next = new ListNode(numList[i]);
-            trav = trav.next;
+  ListNode trav = head;
+  List < int > numList = new List < int > ();
+  numList.Add(head.val);
 
-        }
-        return returnNode;
+  while (trav.next != null) {
+   trav = trav.next;
+   numList.Add(trav.val);
+  }
+  numList.Reverse();
+  ListNode returnNode = new ListNode(numList[0]);
+  trav = returnNode;
 
 
-    }
+  for (int i = 1; i < numList.Count; i++) {
+   trav.next = new ListNode(numList[i]);
+   trav = trav.next;
+
+  }
+  return returnNode;
+
+
+ }
 }
