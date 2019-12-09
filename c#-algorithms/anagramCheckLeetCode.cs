31a646b701d4fbeff3239258d773f5cbae8ac1c4
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-08 21:01:05 -0500

    fully functional, prettify and final commit

--- c#-algorithms/anagramCheckLeetCode.cs
+++ c#-algorithms/anagramCheckLeetCode.cs
@@ -1,28 +1,22 @@
 public class Solution {
  public bool IsAnagram(string s, string t) {
-     
- if (s.Length != t.Length)
-     return false;
+
+  if (s.Length != t.Length)
+   return false;
 
   char[] sLetters = s.ToArray();
   char[] tLetters = t.ToArray();
-     
 
   Array.Sort(sLetters);
   Array.Sort(tLetters);
- 
-   
-  {
 
-   for (int i = 0; i < tLetters.Length; i++) {
-    if (sLetters[i] != tLetters[i]){
-        return false;
-    }
+  for (int i = 0; i < tLetters.Length; i++) {
+   if (sLetters[i] != tLetters[i])
+    return false;
+  }
+  return true;
 
-   }
-   return true;
-
-  } 
  }
+
 
 }
