ae6fcf7d5673b9b854b6e299368a1ca725c579b4
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-09 22:07:59 -0500

    optimized and prettified, looking for faster alt solution

--- c#-algorithms/firstUniqueIndexLeetCode.cs
+++ c#-algorithms/firstUniqueIndexLeetCode.cs
@@ -1,21 +1,21 @@
 public class Solution {
-    public int FirstUniqChar(string s) {
-        
-        char[] letters = s.ToCharArray();
-        
-        
-        foreach(char letter in letters){
-            
-            int firstIndex = Array.IndexOf(letters, letter);
-            int lastIndex = Array.LastIndexOf(letters, letter);
+ public int FirstUniqChar(string s) {
 
-            if(firstIndex == lastIndex){
-                   return firstIndex;
-            }
-             
-       
-        }
-        return -1;
-        
-    }
+  char[] letters = s.ToCharArray();
+
+
+  foreach(char letter in letters) {
+
+   int firstIndex = Array.IndexOf(letters, letter);
+
+
+   if (firstIndex == Array.LastIndexOf(letters, letter)) {
+    return firstIndex;
+   }
+
+
+  }
+  return -1;
+
+ }
 }
