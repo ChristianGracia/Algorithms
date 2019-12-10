252f2eaab24a5f319641f8fc684f0ea2ea01939c
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-09 20:03:43 -0500

    algorithm fully complete in c#

--- c#-algorithms/squareArraySortLeetCode.cs
+++ c#-algorithms/squareArraySortLeetCode.cs
@@ -1,16 +1,13 @@
 public class Solution {
     public int[] SortedSquares(int[] A) {
         
-         int[] squareArr = new int[A.Length];
-        for (int i = 0; i < A.Length; i++){
+        int[] squareArr = new int[A.Length];
+        for (int i = 0; i < A.Length; i++)
+        {
             squareArr[i] = (int) Math.Pow(A[i], 2);
         }
-        foreach(int item in squareArr){
-            Console.WriteLine(item);
-        }
-        
+        Array.Sort(squareArr);
         return squareArr;
-        
-        
+   
     }
 }
