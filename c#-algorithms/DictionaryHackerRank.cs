7fbf36e86249072b1cfda32d817c75be8cb7b7e4
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-10 23:12:38 -0500

    add to dictionary set up

--- c#-algorithms/DictionaryHackerRank.cs
+++ c#-algorithms/DictionaryHackerRank.cs
@@ -5,9 +5,14 @@ class Solution {
     static void Main(String[] args) {
         int numberOfArgs = Convert.ToInt32(Console.ReadLine());
 
+        Dictionary<int,string> dictionary = new Dictionary<int, string>();
+
         for(int i = 0; i < numberOfArgs; i++){
-            string inputString = Console.ReadLine();
+            string[] inputString = Console.ReadLine().Split();
+            Console.WriteLine(inputString[0]);
+            Console.WriteLine(inputString[1]);
+
+
         }
     }
 }
-
