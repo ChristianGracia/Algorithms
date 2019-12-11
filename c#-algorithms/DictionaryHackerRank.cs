07797ce012081ad68cba5dd4b096d5ea19b52148
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-10 23:27:44 -0500

    algorithm fully function for hacker rank

--- c#-algorithms/DictionaryHackerRank.cs
+++ c#-algorithms/DictionaryHackerRank.cs
@@ -2,33 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 class Solution {
-    static void Main(String[] args) {
-        int numberOfArgs = Convert.ToInt32(Console.ReadLine());
+ static void Main(String[] args) {
+  int numberOfArgs = Convert.ToInt32(Console.ReadLine());
 
-        Dictionary<string,string> dictionary = new Dictionary<string, string>();
+  Dictionary < string, string > dictionary = new Dictionary < string, string > ();
 
-        for(int i = 0; i < numberOfArgs; i++){
-            string[] inputString = Console.ReadLine().Split();
-            dictionary.Add(inputString[0], inputString[1]);
-        }
+  for (int i = 0; i < numberOfArgs; i++) {
+   string[] inputString = Console.ReadLine().Split();
+   dictionary.Add(inputString[0], inputString[1]);
+  }
 
 
 
-        List<string> nameList = new List<string>();
+  List < string > nameList = new List < string > ();
 
-      string name = "";
-     while (!string.IsNullOrEmpty(name = Console.ReadLine())){
-        nameList.Add(name);
-      }
+  string name = "";
+  while (!string.IsNullOrEmpty(name = Console.ReadLine())) {
+   nameList.Add(name);
+  }
 
-      foreach(string names in nameList){
-          Console.WriteLine(names);
-      }
-          
+  string result;
+  foreach(string names in nameList) {
+   if (dictionary.TryGetValue(names, out result)) {
+    Console.WriteLine(names + "=" + result);
+   } else {
+    Console.WriteLine("Not found");
+   }
+  }
 
-        
-    }
 
+
+ }
 }
-
 
