4f3f54358595628fe849d77ce877e3ca9f04bfc6
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-10 23:20:34 -0500

    algorithm more set up

--- c#-algorithms/DictionaryHackerRank.cs
+++ c#-algorithms/DictionaryHackerRank.cs
@@ -5,14 +5,27 @@ class Solution {
     static void Main(String[] args) {
         int numberOfArgs = Convert.ToInt32(Console.ReadLine());
 
-        Dictionary<int,string> dictionary = new Dictionary<int, string>();
+        Dictionary<string,string> dictionary = new Dictionary<string, string>();
 
         for(int i = 0; i < numberOfArgs; i++){
             string[] inputString = Console.ReadLine().Split();
-            Console.WriteLine(inputString[0]);
-            Console.WriteLine(inputString[1]);
-
-
+            dictionary.Add(inputString[0], inputString[1]);
         }
+
+        string name = Console.ReadLine();
+
+        ArrayList<string> nameList = new ArrayList<string>();
+
+    
+      while(Console.ReadLine()){
+          
+
+      }
+          
+
+        
     }
+
 }
+
+
