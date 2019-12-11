c29610e07437d4756f4823f8e32a9afd70b0e02a
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-10 21:01:29 -0500

    hackerrank algorithm using dictionaries init

---
+++ c#-algorithms/DictionaryHackerRank.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        int numberOfArgs = Convert.ToInt32(Console.ReadLine());
+
+        for(int i = 0; i < numberOfArgs; i++){
+            string inputString = Console.ReadLine();
+        }
+    }
+}
+
