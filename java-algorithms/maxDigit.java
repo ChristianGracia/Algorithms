2a6a085d4aad58b815d202d0b8ab7d34bd1269b1
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-23 22:37:30 -0500

    max digit in a num algorithm done in java

---
+++ java-algorithms/maxDigit.java
@@ -0,0 +1,19 @@
+int maxDigit(int n) {
+    int c = n;
+    int highestDigit = 0;
+    System.out.println(n%10);
+    
+    while(c > 9){
+            if(c % 10 > highestDigit){
+        highestDigit = c % 10;
+         
+    }
+        
+               c = c/ 10;
+    }
+    
+
+    
+    return highestDigit;
+
+}
