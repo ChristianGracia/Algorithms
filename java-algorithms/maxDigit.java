10453ee9b8a0693f305f980689f8d80705fecee4
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-23 23:03:48 -0500

    prettified

--- java-algorithms/maxDigit.java
+++ java-algorithms/maxDigit.java
@@ -1,19 +1,19 @@
 int maxDigit(int n) {
-    int c = n;
-    int highestDigit = 0;
-    System.out.println(n%10);
-    
-    while(c > 9){
-            if(c % 10 > highestDigit){
-        highestDigit = c % 10;
-         
-    }
-        
-               c = c/ 10;
-    }
-    
+ int c = n;
+ int highestDigit = 0;
+ System.out.println(n % 10);
 
-    
-    return highestDigit;
+ while (c > 9) {
+  if (c % 10 > highestDigit) {
+   highestDigit = c % 10;
+
+  }
+
+  c = c / 10;
+ }
+
+
+
+ return highestDigit;
 
 }
