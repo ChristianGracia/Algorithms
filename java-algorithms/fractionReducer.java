e0df9ae12169db5ee02d8d39fcd1a7d95f67261b
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-25 22:45:57 -0500

    added fraction reducer algorithm in java

---
+++ java-algorithms/fractionReducer.java
@@ -0,0 +1,10 @@
+int[] fractionReducing(int[] fraction) {
+    for(int i = 2; i < fraction[1]; i++){
+        if(fraction[0] % i == 0 && fraction[1] % i == 0){
+            fraction[0] = fraction[0] % i;
+               fraction[1] = fraction[1] % i;
+        }
+    }
+    return fraction;
+
+}
