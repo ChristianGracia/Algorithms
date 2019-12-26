ec0d7729998d29c2f303b53fe6f2340ecee3ab4d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-25 23:29:09 -0500

    fraction reducing algorithm passing all test cases in java

--- java-algorithms/fractionReducer.java
+++ java-algorithms/fractionReducer.java
@@ -1,5 +1,10 @@
 int[] fractionReducing(int[] fraction) {
-    for(int i = 2; i < fraction[1]; i++){
+    
+    int length = fraction[0];
+    if (fraction[1] > fraction[0]){
+        length = fraction[1];
+    }
+    for(int i = 2; i < length; i++){
         if(fraction[0] % i == 0 && fraction[1] % i == 0){
             fraction[0] = fraction[0] / i;
             fraction[1] = fraction[1] / i;
