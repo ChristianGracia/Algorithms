eeeef82ad98baebd3ffdaf87e6b35c1c20219de3
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-03 01:30:56 -0500

    algorithm fully functional

--- java-algorithms/removeDuplicateString.java
+++ java-algorithms/removeDuplicateString.java
@@ -2,8 +2,11 @@ String[] removeDuplicateStrings(String[] inputArray) {
     ArrayList<String> list = new ArrayList<String>();
     
     for (String s : inputArray){
-        if(list.)
+        if(!list.contains(s)){
+            list.add(s);
+        }
     }
-    return list.toArray();
+    
+    return  list.toArray(new String[list.size()]);
 
 }
