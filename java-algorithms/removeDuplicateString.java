6b1f3d323985448938684fec7cc40fa7059cfee8
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-02 20:36:42 -0500

    algorithm in java init

---
+++ java-algorithms/removeDuplicateString.java
@@ -0,0 +1,9 @@
+String[] removeDuplicateStrings(String[] inputArray) {
+    ArrayList<String> list = new ArrayList<String>();
+    
+    for (String s : inputArray){
+        if(list.)
+    }
+    return list.toArray();
+
+}
