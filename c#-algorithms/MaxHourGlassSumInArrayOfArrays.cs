30e825118d30242d54287f7e00ea5b958cad74b7
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-04 22:34:26 -0500

    working on finding max hour glasse shape sum in array of arrays in c#

---
+++ c#-algorithms/MaxHourGlassSumInArrayOfArrays.cs
@@ -0,0 +1,63 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+
+
+    static void Main(string[] args)
+    {
+        int[][] arr = new int[6][];
+        int max = 0;
+
+        for (int i = 0; i < 6; i++)
+        {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (i % 2 == 0)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    int count = 0;
+                    for (int l = k; l < i + 3; l++)
+                    {
+                        count += arr[i][l];
+                        count += arr[i + 2][l];
+                    }
+                    count += arr[i + 1][k + 1];
+
+                    if (count > max)
+                    {
+                        max = count;
+                    }
+
+
+
+                }
+
+
+            }
+
+
+        }
+        Console.WriteLine(max);
+
+
+
+    }
+}
