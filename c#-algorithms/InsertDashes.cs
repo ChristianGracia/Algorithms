bfe24e564082b17731926fd0e50c4a9a63337bc1
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-04 23:34:52 -0500

    algorithm almost complete

--- c#-algorithms/InsertDashes.cs
+++ c#-algorithms/InsertDashes.cs
@@ -1,4 +1,19 @@
-String insertDashes(String inputString)
+string insertDashes(string inputString)
 {
+    string returnString = "";
+    foreach (char item in inputString)
+    {
+        if (item != ' ')
+        {
+            returnString += item;
+            returnString += "-";
+        }
+        else
+        {
+            returnString += item;
+        }
+
+    }
+    return returnString;
 
 }
