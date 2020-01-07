7063c0f00dfe74f7163e5858e6e734e6af5b7c07
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-06 23:28:17 -0500

    cleaned and removed var

--- c#-algorithms/LetterRepeater/Program.cs
+++ c#-algorithms/LetterRepeater/Program.cs
@@ -6,12 +6,12 @@ namespace LetterRepeater
     {
         static void Main(string[] args)
         {
-            var newString = "";
+            string newString = "";
             Console.WriteLine("Enter a string to repeat each letter");
-            var inputString = Console.ReadLine();
+            string inputString = Console.ReadLine();
 
             Console.WriteLine("Enter how many times you want each letter repeated");
-            var factor = Convert.ToInt32(Console.ReadLine());
+            int factor = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("word: " + inputString + "\nn = " + factor);
 
