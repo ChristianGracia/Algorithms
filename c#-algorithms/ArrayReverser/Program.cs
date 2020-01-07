33eea6d11a3ada07d6ecf66203b96766ff18ce8c
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-06 23:24:41 -0500

    cleaned algorthim, removed var

--- c#-algorithms/ArrayReverser/Program.cs
+++ c#-algorithms/ArrayReverser/Program.cs
@@ -8,22 +8,22 @@ namespace ArrayReverser
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter name to be reversed");
-            var word = Console.ReadLine();
-            var reversedWord = ReverseString(word);
+            string word = Console.ReadLine();
+            string reversedWord = ReverseString(word);
             Console.WriteLine("reversed word is: " + reversedWord);
         }
 
         public static string ReverseString(string name)
         {
-            var length = name.Length;
-            var array = new char[length];
+            int length = name.Length;
+            char[] array = new char[length];
 
             for (var i = 0; i < length; i++)
             {
                 array[i] = name[i];
             }
             Array.Reverse(array);
-            var reversedString = array.Aggregate("", (current, item) => current + (char)item);
+            string reversedString = array.Aggregate("", (current, item) => current + (char)item);
 
             return reversedString;
 
