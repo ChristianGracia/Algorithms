eb492bb10d0789c548fd1a15c0cd8639a1b47bc2
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-15 23:34:40 -0500

    added a one line solution

--- javascript-algorithms/filterOutGivenIndicesOfArray.js
+++ javascript-algorithms/filterOutGivenIndicesOfArray.js
@@ -6,3 +6,7 @@ function removeArrayPart(inputArray, l, r) {
     let newArr = inputArray.filter(x => x != null)
     return newArr;
 }
+
+function removeArrayPart(inputArray, l, r) {
+    return inputArray.filter((x, i) => i < l || i > r);
+}
