355931cfee2044f6f006c988859e1be83609daf3
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-26 20:39:16 -0500

    algorithm can now find first occurence of a string in another string

--- c#-algorithms/StringComparerCounter.cs
+++ c#-algorithms/StringComparerCounter.cs
@@ -1,5 +1,30 @@
 int stringsConstruction(string a, string b)
 {
+    List<char> letters = new List<char>();
+    int counter = 0;
+    foreach (char letter in b)
+    {
+        letters.Add(letter);
+    }
+    foreach (char letter in b)
+    {
+        Console.WriteLine(letter);
+    }
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (i == a.Length - 1)
+        {
+            counter++;
+
+
+        }
+        if (letters.Contains(a[i]))
+        {
+            letters.Remove(a[i]);
+        }
+
+    }
+    return counter;
 
 
 }
