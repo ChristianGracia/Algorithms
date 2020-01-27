519dcefff9aa5b48883c1a9b15418ebded79274d
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-26 21:38:04 -0500

    var names refactored, algorithm takes a string and sees how many of a given string can be crcreated using only each letter in the first string once

--- c#-algorithms/StringComparerCounter.cs
+++ c#-algorithms/StringComparerCounter.cs
@@ -3,18 +3,18 @@ int stringsConstruction(string a, string b)
     int counter = 0;
     int[] letters = new int[26];
 
-    foreach (char c in b)
+    foreach (char letter in b)
     {
-        int letterIndex = System.Convert.ToInt32(c) - 97;
+        int letterIndex = System.Convert.ToInt32(letter) - 97;
         letters[letterIndex]++;
 
     }
 
     while (true)
     {
-        for (int i = 0; i < a.Length; i++)
+        for (int index = 0; index < a.Length; index++)
         {
-            int letterIndex = System.Convert.ToInt32(a[i]) - 97;
+            int letterIndex = System.Convert.ToInt32(a[index]) - 97;
             if (letters[letterIndex] > 0)
             {
                 letters[letterIndex]--;
@@ -34,3 +34,13 @@ int stringsConstruction(string a, string b)
     return counter;
 
 }
+
+// Given two strings a and b, both consisting only of lowercase English letters, your task is to calculate how many strings equal to a can be constructed using only letters from the string b? Each letter can be used only once and in one string only.
+
+// Example
+
+// For a = "abc" and b = "abccba", the output should be stringsConstruction(a, b) = 2.
+
+// We can construct 2 strings a = "abc" using only letters from the string b.
+
+// For a = "ab" and b = "abcbcb", the output should be stringsConstruction(a, b) = 1.
