fe04b970522b11ab18a7b333c287b3e3025e5372
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-26 21:35:36 -0500

    algorithm fully functional in c#

--- c#-algorithms/StringComparerCounter.cs
+++ c#-algorithms/StringComparerCounter.cs
@@ -1,30 +1,36 @@
 int stringsConstruction(string a, string b)
 {
-    List<char> letters = new List<char>();
     int counter = 0;
-    foreach (char letter in b)
-    {
-        letters.Add(letter);
-    }
-    foreach (char letter in b)
-    {
-        Console.WriteLine(letter);
-    }
-    for (int i = 0; i < a.Length; i++)
-    {
-        if (i == a.Length - 1)
-        {
-            counter++;
+    int[] letters = new int[26];
 
+    foreach (char c in b)
+    {
+        int letterIndex = System.Convert.ToInt32(c) - 97;
+        letters[letterIndex]++;
+
+    }
+
+    while (true)
+    {
+        for (int i = 0; i < a.Length; i++)
+        {
+            int letterIndex = System.Convert.ToInt32(a[i]) - 97;
+            if (letters[letterIndex] > 0)
+            {
+                letters[letterIndex]--;
+            }
+            else
+            {
+                return counter;
+            }
 
         }
-        if (letters.Contains(a[i]))
-        {
-            letters.Remove(a[i]);
-        }
+        counter++;
 
     }
+
+
+
     return counter;
-
 
 }
