019a08063b7b96be6cf8abdc7ab8af962f9eeaa0
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-26 21:39:10 -0500

    shortened

--- c#-algorithms/StringComparerCounter.cs
+++ c#-algorithms/StringComparerCounter.cs
@@ -28,11 +28,6 @@ int stringsConstruction(string a, string b)
         counter++;
 
     }
-
-
-
-    return counter;
-
 }
 
 // Given two strings a and b, both consisting only of lowercase English letters, your task is to calculate how many strings equal to a can be constructed using only letters from the string b? Each letter can be used only once and in one string only.
