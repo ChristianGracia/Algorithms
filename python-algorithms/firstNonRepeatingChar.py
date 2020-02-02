fc7f0475d6aa1fed991fc0128cf96b67d8a49f9d
Christian Gracia <christiangracia.student@careerdevs.com>
2020-02-02 15:38:28 -0500

    completed the first non repeating char algorithm I just wrote in C# in python

---
+++ python-algorithms/firstNonRepeatingChar.py
@@ -0,0 +1,20 @@
+def firstNotRepeatingCharacter(s):
+    for x in s:
+        if s.index(x) == s.rindex(x):
+            return x
+    return '_'
+
+
+# Given a string s consisting of small English letters, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
+
+# Example
+
+# For s = "abacabad", the output should be
+# firstNotRepeatingCharacter(s) = 'c'.
+
+# There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it appears in the string first.
+
+# For s = "abacabaabacaba", the output should be
+# firstNotRepeatingCharacter(s) = '_'.
+
+# There are no characters in this string that do not repeat.
