fb04c4e78be303734f6dc4e11f39bc5b589353a9
Christian Gracia <christiangracia.student@careerdevs.com>
2020-02-01 22:06:00 -0500

    added shorter solution in c# for even odd check

--- c#-algorithms/EvenOddChecker.cs
+++ c#-algorithms/EvenOddChecker.cs
@@ -1,6 +1,17 @@
 string characterParity(char symbol)
 {
 
+    if (symbol < '0' || symbol > '9')
+        return "not a digit";
+    if ((symbol - '0') % 2 == 0)
+        return "even";
+    return "odd";
+}
+
+
+string characterParity(char symbol)
+{
+
     if (!char.IsLetter(symbol))
     {
         if (Convert.ToInt32(symbol) % 2 == 0)
