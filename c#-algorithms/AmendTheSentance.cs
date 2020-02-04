f39693d3232466daf68a13b49ed163dbc172bd0d
Christian Gracia <christiangracia.student@careerdevs.com>
2020-02-03 22:04:33 -0500

    added algorithm prompt

--- c#-algorithms/AmendTheSentance.cs
+++ c#-algorithms/AmendTheSentance.cs
@@ -10,3 +10,9 @@ string amendTheSentence(string s)
     }
     return s.Trim();
 }
+
+// You have been given a string s, which is supposed to be a sentence.However, someone forgot to put spaces between the different words, and for some reason they capitalized the first letter of every word.Return the sentence after making the following amendments:
+
+
+// Put a single space between the words.
+// Convert the uppercase letters to lowercase.
