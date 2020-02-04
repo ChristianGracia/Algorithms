7c036fe40190fe5e571ff8a68e306cb9c0e5b1a0
Christian Gracia <christiangracia.student@careerdevs.com>
2020-02-03 22:05:37 -0500

    shortened code, algorithm fully functional in c#

--- c#-algorithms/AmendTheSentance.cs
+++ c#-algorithms/AmendTheSentance.cs
@@ -3,11 +3,7 @@ string amendTheSentence(string s)
     string pattern = @"[A-Z\s]";
     Regex rgx = new Regex(pattern);
     foreach (Match match in rgx.Matches(s))
-    {
-
         s = Regex.Replace(s, match.Value, " " + match.Value.ToLower());
-
-    }
     return s.Trim();
 }
 
