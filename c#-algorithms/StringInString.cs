b76aeb0ff5807cded8204193caf4f0e0b672f032
Christian Gracia <christiangracia.student@careerdevs.com>
2020-02-05 21:11:06 -0500

    started an algorithm in c# that checks if a string is contained in another string and returns the index of the first occurence, currently passing most test cases

---
+++ c#-algorithms/StringInString.cs
@@ -0,0 +1,31 @@
+int strstr(string s, string x) {
+    int counter = 0;
+    int index = 0;
+    foreach(char letter in s){
+        index++;
+        if(letter == x[counter]){
+            counter++;
+            if(counter == x.Length -1){
+                return index - counter;
+            }
+        }
+        else{
+            counter = 0;
+        }
+    }
+    return -1;
+
+
+}
+
+
+// Avoid using built-in functions to solve this challenge. Implement them yourself, since this is what you would be asked to do during a real interview.
+
+// Implement a function that takes two strings, s and x, as arguments and finds the first occurrence of the string x in s. The function should return an integer indicating the index in s of the first occurrence of x. If there are no occurrences of x in s, return -1.
+
+// Example
+
+// For s = "CodefightsIsAwesome" and x = "IA", the output should be
+// strstr(s, x) = -1;
+// For s = "CodefightsIsAwesome" and x = "IsA", the output should be
+// strstr(s, x) = 10.
