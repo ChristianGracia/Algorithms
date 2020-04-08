ef79d4fd4f4b288fabd626e534625b9cbe41cf14
Christian Gracia <christiangracia.student@careerdevs.com>
2020-04-08 19:42:12 -0400

    added shapeArea algorithm done in ruby

---
+++ ruby-algorithms/shapeArea.rb
@@ -0,0 +1,20 @@
+def shapeArea(n)
+
+$area = 1
+$limit = n - 1
+
+while $limit > 0 do
+
+        $area += (($limit) * 4)
+    $limit -= 1
+end
+return $area
+end
+
+
+# Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+
+# A 1-interesting polygon is just a square with a side of length 1. 
+# An n-interesting polygon is obtained by taking the n - 1-interesting polygon and 
+# appending 1-interesting polygons to its rim, side by side.
+# You can see the 1-, 2-, 3- and4-interesting polygons in the picture below.
