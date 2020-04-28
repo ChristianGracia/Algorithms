429b1c6b9c5e68b7c3f13f5b25adc84bb1f7f445
Christian Gracia <christiangracia.student@careerdevs.com>
2020-04-27 21:05:22 -0400

    added input algorithm done in python

---
+++ python-algorithms/numberReader.py
@@ -0,0 +1,22 @@
+
+number = 1
+numList = []
+numPositives = 0
+numNegatives = 0
+average = 0
+
+while number != 0:
+    number = int(input("Enter an integer, the input ends if it is 0: "))
+    if(number != 0):
+        numList.append(number)
+        if (number % 2 != 0):
+            numNegatives += 1
+        else:
+            numPositives += 1
+
+average = float(sum(numList, 0) / len(numList))
+
+print("The number of positives is " + str(numPositives))
+print("The number of negatives is " + str(numNegatives))
+print("The total is " + str(len(numList)))
+print("The average is " + str(average))
