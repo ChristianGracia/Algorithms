ea1911013d557979a676c87d79a2681b53c4a04c
Christian Gracia <christiangracia.student@careerdevs.com>
2020-04-27 21:07:24 -0400

    changed to count negative/pos values not even/odd

--- python-algorithms/numberReader.py
+++ python-algorithms/numberReader.py
@@ -1,4 +1,3 @@
-
 number = 1
 numList = []
 numPositives = 0
@@ -9,7 +8,7 @@ while number != 0:
     number = int(input("Enter an integer, the input ends if it is 0: "))
     if(number != 0):
         numList.append(number)
-        if (number % 2 != 0):
+        if (number * -1 > 0):
             numNegatives += 1
         else:
             numPositives += 1
