1f79cda4f06d35405b1b0627d334ed6dd5f1afdc
Christian Gracia <christiangracia.student@careerdevs.com>
2020-05-14 19:37:55 -0400

    passing some test cases

--- python-algorithms/binaryTreeNodeParentPredicter.py
+++ python-algorithms/binaryTreeNodeParentPredicter.py
@@ -3,17 +3,27 @@
 # 1 2 4 5
 
 h = 3
-# q = [1, 4, 7]
+q = [1, 4, 7]
 returnArr = []
 
-q = [7, 3, 5, 1]
+# q = [7, 3, 5, 1]
 
-for treeElement in q:
 
-    if treeElement > h * 2:
-        returnArr.append(-1)
-    elif treeElement % 2 == 0 & treeElement:
-        returnArr.append(treeElement + 1)
-    else:
-        returnArr.append(treeElement + 2)
-print(returnArr)
+def solution(h, q):
+    returnArr = []
+
+    for treeElement in q:
+
+        if treeElement > h * 2:
+            returnArr.append(-1)
+        elif treeElement % 3 == 0:
+            returnArr.append((treeElement * 2)+1)
+        elif (treeElement + 1) % 3 == 0:
+            returnArr.append(treeElement + 1)
+        elif treeElement % 2 == 0:
+            returnArr.append(treeElement + 1)
+        else:
+            returnArr.append(treeElement + 2)
+
+
+return returnArr
