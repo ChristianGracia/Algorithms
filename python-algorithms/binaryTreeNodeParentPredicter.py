1e62b95fbc24033a992f0dac301ba5463c74c639
Christian Gracia <christiangracia.student@careerdevs.com>
2020-05-14 19:31:12 -0400

    added comment

--- python-algorithms/binaryTreeNodeParentPredicter.py
+++ python-algorithms/binaryTreeNodeParentPredicter.py
@@ -1,3 +1,6 @@
+#    7
+#  3   6
+# 1 2 4 5
 
 h = 3
 # q = [1, 4, 7]
@@ -9,7 +12,7 @@ for treeElement in q:
 
     if treeElement > h * 2:
         returnArr.append(-1)
-    elif treeElement % 2 == 0:
+    elif treeElement % 2 == 0 & treeElement:
         returnArr.append(treeElement + 1)
     else:
         returnArr.append(treeElement + 2)
