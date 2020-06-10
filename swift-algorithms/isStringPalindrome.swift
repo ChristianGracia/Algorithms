34897a1a531cd2f70e540a6e6cbd3489c53ec94c
Christian Gracia <christiangracia.student@careerdevs.com>
2020-06-10 00:48:20 -0400

    check palindrome algorithm done in swift

--- swift-algorithms/isStringPalindrome.swift
+++ swift-algorithms/isStringPalindrome.swift
@@ -1,12 +1,13 @@
 func checkPalindrome(inputString: String) -> Bool {
+    
+    var reversedArray = Array(inputString)
+    var reversedString = String(reversedArray.reversed())
     print(inputString)
-    var check = true
-    var length = 
-
-    for letter in inputString {
-        if(letter)
-
+    print(reversedString)
+    
+    if(inputString == reversedString){
+        return true
     }
- 
-
+    return false
+    
 }
