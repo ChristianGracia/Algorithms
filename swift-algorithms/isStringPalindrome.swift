1ff0446999ab399780e8d54eb06ec28b34523fca
Christian Gracia <christiangracia.student@careerdevs.com>
2020-06-10 00:48:39 -0400

    added specs

--- swift-algorithms/isStringPalindrome.swift
+++ swift-algorithms/isStringPalindrome.swift
@@ -11,3 +11,16 @@ func checkPalindrome(inputString: String) -> Bool {
     return false
     
 }
+
+
+// Given the string, check if it is a palindrome.
+
+// Example
+
+// For inputString = "aabaa", the output should be
+// checkPalindrome(inputString) = true;
+// For inputString = "abac", the output should be
+// checkPalindrome(inputString) = false;
+// For inputString = "a", the output should be
+// checkPalindrome(inputString) = true.
+// Input/O
