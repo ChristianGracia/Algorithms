5c4df351c40e417b906e0266581369bb79df5981
Christian Gracia <christiangracia.student@careerdevs.com>
2020-11-10 23:02:21 -0500

    added reverse order of words in a sentance algorithm in rust

---
+++ rust-algorithms/reverse_a_sentance.rs
@@ -0,0 +1,15 @@
+// Reverse the order of words in a given string sentence. You can assume that sentence does not have any leading, trailing or repeating spaces.
+
+// Example
+
+// For sentence = "Man bites dog", the output should be
+// reverseSentence(sentence) = "dog bites Man".
+
+fn reverseSentence(sentance: String) -> String {
+    let mut mutableSentance: Vec<&str> = sentance.split(" ").collect();
+    
+    mutableSentance.reverse();
+    mutableSentance.join(" ");
+
+    return mutableSentance.join(" ")
+}
--- rust-algorithms/reverse_a_string.rs
+++ rust-algorithms/reverse_a_string.rs
@@ -1,3 +1,5 @@
+// given "dog likes to eat" -> return "tae ot sekil god"
+
 fn reverseString(stringParam: String) -> String {
     stringParam.chars().rev().collect::<String>()
 }
