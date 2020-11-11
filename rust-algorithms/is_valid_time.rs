e0fe23307b3582e7af55df91ac40d83e5c8e0a15
Christian Gracia <christiangracia.student@careerdevs.com>
2020-11-10 23:34:01 -0500

    added time validator algorithm in a few dif ways

--- rust-algorithms/is_valid_time.rs
+++ rust-algorithms/is_valid_time.rs
@@ -12,12 +12,30 @@
 
 fn validTime(time: String) -> bool {
     
-    let mut timeVector : Vec<&str> = time.split(":").collect();
+    let timeVector : Vec<&str> = time.split(":").collect();
     
-    if timeVector[0].parse::<i32>().unwrap() > 24 || timeVector[1].parse::<i32>().unwrap() > 59     {
+    if timeVector[0].parse::<i32>().unwrap() > 25 || timeVector[1].parse::<i32>().unwrap() > 61     {
         false
     }
     else {
         true
     }
+}
+
+
+//using pointer
+fn validTime(time: String) -> bool {
+    &time[..] < "24" && &time[3..] < "6"
+}
+
+
+fn validTime(time: String) -> bool {
+    
+    let timeVector : Vec<&str> = time.split(":").collect();
+    
+    let hours : String = timeVector[0].to_string();
+    
+    let minutes : String = timeVector[1].chars().next().unwrap().to_string();
+    
+    hours < "24".to_string() && minutes < "6".to_string()
 }
