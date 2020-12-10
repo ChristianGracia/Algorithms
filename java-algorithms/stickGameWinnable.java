adfe43b5ddee2a87c2179c17cc7e41984317509b
Christian Gracia <christiangracia.student@careerdevs.com>
2020-12-09 21:17:16 -0500

    java algorithm more complete

--- java-algorithms/stickGameWinnable.java
+++ java-algorithms/stickGameWinnable.java
@@ -15,7 +15,7 @@ public class Main {
         System.out.println(win(3, moveChoices ));
 
         //with 5
-//        win(5, moveChoices );
+        System.out.println(win(5, moveChoices ));
 
 
 
@@ -28,6 +28,19 @@ public class Main {
         return (index < 0) ? -1 : index;
     }
 
+    public static boolean checkIfNextMoveCanWin(int currentSticks, int[] arr){
+        return getIndex(currentSticks, arr) > -1 ;
+    }
+
+    public static boolean checkIfNoAvailableMoves(int currentSticks, int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] <= currentSticks){
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static boolean win(int numberOfSticks, int[] moveChoices){
 
         int arrLength = moveChoices.length;
@@ -35,39 +48,44 @@ public class Main {
         int currentSticks = numberOfSticks;
         int[] tempMoveArr = moveChoices;
 
-        boolean gameIsWinnable = false;
 
-        //check for auto win on first move and remove values greater then number of sticks
-        for(int i = 0; i < arrLength; i ++){
-            if(tempMoveArr[i] == numberOfSticks){
-                return true;
-            }
-        }
-        System.out.println(tempMoveArr);
+        for(int i = 0; i < arrLength; i++) {
+            for (int j = 0; j < arrLength; j++) {
+                if (i == j) {
+                    j++;
+                }
+
+                if (checkIfNextMoveCanWin(currentSticks, tempMoveArr)) {
+                    return true;
+                }
+                if (checkIfNoAvailableMoves(currentSticks, tempMoveArr)) {
+                    return false;
+                }
+
+                if (tempMoveArr[i] <= currentSticks) {
+                    currentSticks -= tempMoveArr[i];
+                    System.out.println("current sticks " + currentSticks);
+                } else {
+
+                    System.out.println("move: " + tempMoveArr[i] + " " + " current sticks: " + currentSticks);
+                    tempMoveArr[i] = 0;
+                }
+
+                if (currentSticks == 0) {
+                    return true;
+                }
+                if (checkIfNextMoveCanWin(currentSticks, tempMoveArr)) {
+                    return false;
+                }
+                if (checkIfNoAvailableMoves(currentSticks, tempMoveArr)) {
+                    return true;
+                }
 
 
-        for(int i = 0; i < arrLength; i++){
-            if (tempMoveArr[i] <= currentSticks){
-                currentSticks -= tempMoveArr[i];
-                System.out.println("current sticks" + currentSticks);
-                
-            }
-            else{
-                tempMoveArr[i] = 0;
-                System.out.println("move: " + tempMoveArr[i]  + " " + " current sticks: " + currentSticks);
-            }
-            if(currentSticks == 0){
-                return true;
             }
             tempMoveArr = moveChoices;
+            currentSticks = numberOfSticks;
         }
-
-
-
-
         return false;
-
-
-
     }
 }
