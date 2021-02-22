dd02a50bcd7363ca356dbd90af203bc08665c725
Christian Gracia <christiangracia.student@careerdevs.com>
2021-02-21 22:18:19 -0500

    added password gen algorithm in swift

---
+++ swift-algorithms/passwordGenerator.swift
@@ -0,0 +1,13 @@
+func passworsGenerator() {
+   
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+   
+    var password = ""
+   
+    for _ in 0...5 {
+        password += (alphabet.randomElement() ?? "") as String
+    }
+   
+    print(password)
+   
+}
