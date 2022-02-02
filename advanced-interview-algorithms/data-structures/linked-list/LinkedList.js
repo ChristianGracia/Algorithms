d22ae7f5b4985d400520563e43b539a07525d67b
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-02-02 00:08:27 -0600

    added traversal and insert methods to linked list class

--- advanced-interview-algorithms/data-structures/linked-list/LinkedList.js
+++ advanced-interview-algorithms/data-structures/linked-list/LinkedList.js
@@ -23,17 +23,53 @@ class LinkedList {
     }
 
     prepend(value) {
-        const newNode = new Node(value);;
+        const newNode = new Node(value);
         newNode.next = this.head;
         this.head = newNode;
         this.length++;
+    }
+
+    insert(index, value) {
+        if (index >= this.length) {
+            return this.append(value);
+        } else {
+            const newNode = new Node(value);
+            const precedingNode = this.traverseToIndex(index-1);
+            const tmp = precedingNode.next;
+            precedingNode.next = newNode;
+            newNode.next = tmp;
+            this.length++;
+            return this;
+        }
+    }
+    traverseToIndex(number) {
+        let counter = 0;
+        let trav = this.head;
+        while(counter !== number) {
+            trav = trav.next;
+            counter++;
+        }
+        return trav;
+    }
+
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
     }
 }
 
 const linkedList = new LinkedList(10);
 linkedList.append(5);
 linkedList.append(14);
-console.log(linkedList)
 
+
+console.log(linkedList.printList());
 linkedList.prepend(1);
-console.log(linkedList)
+console.log(linkedList.printList());
+linkedList.insert(2, 333333)
+console.log(linkedList.printList());
