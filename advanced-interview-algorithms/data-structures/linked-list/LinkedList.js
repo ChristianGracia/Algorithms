763f107de746cd0bb57dc522f7dbb8c9b601f284
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-02-01 22:52:03 -0600

    added some directories and a basic js linked-list class

---
+++ advanced-interview-algorithms/data-structures/linked-list/LinkedList.js
@@ -0,0 +1,38 @@
+class LinkedList {
+    constructor(value) {
+        this.head = {
+            value,
+            next: null
+        }
+        this.tail = this.head;
+        this.length = 1;
+    }
+    append(value) {
+        const newNode = {
+            value,
+            next: null
+        }
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+
+    prepend(value) {
+        const newNode = {
+            value,
+            next: null
+        };
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+    }
+}
+
+const linkedList = new LinkedList(10);
+linkedList.append(5);
+linkedList.append(14);
+console.log(linkedList)
+
+linkedList.prepend(1);
+console.log(linkedList)
