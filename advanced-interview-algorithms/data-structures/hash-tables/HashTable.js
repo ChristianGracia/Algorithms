543fb35ffcaec41732665a73188055950c8e1f06
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-02-01 21:25:59 -0600

    added keys method with key collions accounted for

--- advanced-interview-algorithms/data-structures/hash-tables/HashTable.js
+++ advanced-interview-algorithms/data-structures/hash-tables/HashTable.js
@@ -37,6 +37,7 @@ class HashTable {
         return this.data;
     }
 
+    // O(n)
     keys() {
         const keysArray = [];
         for(let i = 0; i < this.data.length; i++) {
@@ -45,6 +46,26 @@ class HashTable {
             }
         }
         return keysArray;
+    }
+
+    // O(n)
+    keysWithCollisions() {
+        if (!this.data.length) {
+            return undefined
+          }
+          let result = []
+          for (let i = 0; i < this.data.length; i++) {
+              if (this.data[i] && this.data[i].length) {
+                if (this.data.length > 1) {
+                  for (let j = 0; j < this.data[i].length; j++) {
+                    result.push(this.data[i][j][0])
+                  }
+                } else {
+                  result.push(this.data[i][0])
+                } 
+              }
+          }
+          return result; 
     }
   }
   
@@ -56,3 +77,4 @@ class HashTable {
   hashTable.set('dogs2', 9);
   hashTable.set('dogs3', 9);
   console.log(hashTable.keys());
+  console.log(hashTable.keysWithCollisions());
