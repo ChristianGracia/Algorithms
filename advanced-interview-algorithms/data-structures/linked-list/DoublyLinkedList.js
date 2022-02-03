f51c7de9e908f6eb6f50a2e245c934f60f4bacb3
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-02-02 18:08:16 -0600

    added doubly linked list

--- advanced-interview-algorithms/data-structures/linked-list/DoublyLinkedList.js
+++ advanced-interview-algorithms/data-structures/linked-list/DoublyLinkedList.js
@@ -2,20 +2,23 @@ class Node {
     constructor(value) {
         this.value = value;
         this.next = null;
+        this.prev = null;
     }
 }
 
-class LinkedList {
+class DoublyLinkedList {
     constructor(value) {
         this.head = {
             value,
-            next: null
+            next: null,
+            prev: null
         }
         this.tail = this.head;
         this.length = 1;
     }
     append(value) {
         const newNode = new Node(value);
+        newNode.prev = this.tail;
         this.tail.next = newNode;
         this.tail = newNode;
         this.length++;
@@ -25,6 +28,7 @@ class LinkedList {
     prepend(value) {
         const newNode = new Node(value);
         newNode.next = this.head;
+        this.head.prev = newNode;
         this.head = newNode;
         this.length++;
         return this;
@@ -36,9 +40,12 @@ class LinkedList {
         } else {
             const newNode = new Node(value);
             const prevNode = this.traverseToIndex(index-1);
-            const tmp = prevNode.next;
+            const nextNode = prevNode.next;
             prevNode.next = newNode;
-            newNode.next = tmp;
+            newNode.prev = prevNode;
+            newNode.next = nextNode;
+            nextNode.prev = newNode;
+
             this.length++;
             return this;
         }
@@ -67,20 +74,22 @@ class LinkedList {
         const precedingNode = this.traverseToIndex(index - 1);
         const removedNode = precedingNode.next;
         precedingNode.next = removedNode.next;
+        removedNode.next.prev = precedingNode;
         this.length--;
         return this.printList()
     }
 }
 
-const linkedList = new LinkedList(10);
-linkedList.append(5);
-linkedList.append(14);
+const doubleLinkedList = new DoublyLinkedList(10);
+doubleLinkedList.append(5);
+doubleLinkedList.append(143);
+doubleLinkedList.append(14);
 
 
-console.log(linkedList.printList());
-linkedList.prepend(1);
-console.log(linkedList.printList());
-linkedList.insert(2, 333333)
-console.log(linkedList.printList());
-linkedList.remove(2)
-console.log(linkedList.printList());
+// console.log(doubleLinkedList.printList());
+// console.log(doubleLinkedList.prepend(1));
+// console.log(doubleLinkedList.printList());
+// // doubleLinkedList.insert(2, 333333)
+console.log(doubleLinkedList.printList());
+doubleLinkedList.remove(1)
+console.log(doubleLinkedList.printList());
