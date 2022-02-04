159556dd8fe881c59913d110cc8da4090b418f4e
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-02-03 20:26:59 -0600

    added more rotate array solutions and final

--- advanced-interview-algorithms/javascript/leetCodeRotateArray.js
+++ advanced-interview-algorithms/javascript/leetCodeRotateArray.js
@@ -1,24 +1,50 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {void} Do not return anything, modify nums in-place instead.
- */
- var rotate = function(nums, k) {
-    const obj = {}
-    const check = k % 2 === 0 ? 0 : 1
-    for(let i = 0; i < nums.length; i++) {
-        
-        let index = (i + k) % (nums.length - 1)
-        obj[i] = nums[i];
-        if(i + k >= nums.length - 1) {
-            nums[i] = obj[index];
-        } else {
-            nums[i] = nums[index + check];
-        }
-    }
-    console.log(nums)
-};
+// /**
+//  * @param {number[]} nums
+//  * @param {number} k
+//  * @return {void} Do not return anything, modify nums in-place instead.
+//  */
+//  var rotate = function(nums, k) {
+//     while (k--) {
+//         nums.unshift( nums.pop() );
+//     }
+// };
 
-// rotate([1, 2, 3, 4, 5], 2);
- rotate([-1,-100,3,99], 2);
-//rotate([1, 2, 3, 4, 5, 6, 7], 3);
+// // rotate([1, 2, 3, 4, 5], 2);
+//  rotate([-1,-100,3,99], 2);
+// //rotate([1, 2, 3, 4, 5, 6, 7], 3);
+
+
+// // passes some testcases
+// var rotate = function(nums, k) {
+//     while (k--) {
+//         nums.unshift( nums.pop() );
+//     }
+//     console.log(nums)
+// };
+
+
+
+// // works
+
+
+// var rotate = function(nums, k) {
+//     if (k >= nums.length ) {
+//         k = k % nums.length
+//     }
+//     if ( k === 0 ) {
+//         return nums
+//     }
+//     nums.unshift(...nums.slice(-k))
+//     nums.splice(-k)
+//     return nums
+// };
+
+
+// this also works
+
+var rotate = function(nums, k) {
+    nums.unshift(...nums.splice(- (k % nums.length)));
+    // console.log(...nums.splice(- (k % nums.length)))
+    return nums
+};
+console.log(rotate([1,2,3,4,5], 2))
