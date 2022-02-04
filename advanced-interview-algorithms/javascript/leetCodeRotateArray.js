a0eb37a31acd4daa342d9a645895515efedde136
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-02-03 20:15:07 -0600

    passing more test cases

--- advanced-interview-algorithms/javascript/leetCodeRotateArray.js
+++ advanced-interview-algorithms/javascript/leetCodeRotateArray.js
@@ -5,17 +5,20 @@
  */
  var rotate = function(nums, k) {
     const obj = {}
+    const check = k % 2 === 0 ? 0 : 1
     for(let i = 0; i < nums.length; i++) {
-        let index = (i + k) % (nums.length)
-        if(i + k > nums.length - 1) {
-            nums[i] = obj[i - k];
+        
+        let index = (i + k) % (nums.length - 1)
+        obj[i] = nums[i];
+        if(i + k >= nums.length - 1) {
+            nums[i] = obj[index];
         } else {
-            obj[i] = nums[i];
-            nums[i] = nums[index];
+            nums[i] = nums[index + check];
         }
-        // console.log(nums);
     }
+    console.log(nums)
 };
 
-rotate([1, 2, 3, 4, 5], 2);
-rotate([1, 2, 3, 4, 5, 6, 7], 3);
+// rotate([1, 2, 3, 4, 5], 2);
+ rotate([-1,-100,3,99], 2);
+//rotate([1, 2, 3, 4, 5, 6, 7], 3);
