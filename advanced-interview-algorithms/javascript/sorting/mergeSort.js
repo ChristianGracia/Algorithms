622f0ee2e98cc41671037c9f738e922423718e41
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-02-08 00:07:35 -0600

    added quick sort algorithm

--- advanced-interview-algorithms/javascript/sorting/mergeSort.js
+++ advanced-interview-algorithms/javascript/sorting/mergeSort.js
@@ -20,8 +20,7 @@ function merge(left, right){
     const result = [];
     let leftIndex = 0;
     let rightIndex = 0;
-    while(leftIndex < left.length && 
-          rightIndex < right.length){
+    while(leftIndex < left.length && rightIndex < right.length) {
        if(left[leftIndex] < right[rightIndex]){
          result.push(left[leftIndex]);
          leftIndex++;
@@ -29,7 +28,8 @@ function merge(left, right){
          result.push(right[rightIndex]);
          rightIndex++
       }
-    }  
+    }
+    console.log(left, right) 
     return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
