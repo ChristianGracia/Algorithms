e0e147e928f7e6e7624b58db4b3e17860920a8a3
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-02-08 20:30:21 -0600

    added better solution for leet code problem: word break, using recursion

--- advanced-interview-algorithms/javascript/leetCodeWordBreak.js
+++ advanced-interview-algorithms/javascript/leetCodeWordBreak.js
@@ -1,30 +1,51 @@
-/**
- * @param {string} s
- * @param {string[]} wordDict
- * @return {boolean}
- */
+// https://leetcode.com/problems/word-break
+// /**
+//  * @param {string} s
+//  * @param {string[]} wordDict
+//  * @return {boolean}
+//  */
+// var wordBreak = function (s, wordDict) {
+//   const splitSentance = s.split("");
+//   let temp = "";
+//   for (let i = 0; i < splitSentance.length; i++) {
+//     temp = temp + splitSentance[i];
+//     if (wordDict.includes(temp)) {
+//       temp = "";
+//     }
+//   }
+//   return !temp ? true : false;
+// };
+
+// better solution
+
 var wordBreak = function (s, wordDict) {
-  const splitSentance = s.split("");
-  let temp = "";
-  for (let i = 0; i < splitSentance.length; i++) {
-    temp = temp + splitSentance[i];
-    console.log(temp);
-    if (wordDict.includes(temp)) {
-      console.log("here");
-      temp = "";
+  const map = {};
+  function run(s) {
+    if (map.hasOwnProperty(s)) {
+      return map[s];
+    } else if (s == "") {
+      return true;
     }
+    for (let i = 0; i <= s.length; i++) {
+      if (wordDict.includes(s.slice(i)) && run(s.slice(0, i))) {
+        map[s] = true;
+        return true;
+      }
+    }
+    map[s] = false;
+    return false;
   }
-  return wordDict.includes(temp) || !temp ? true : false;
+  return run(s);
 };
 
-// let sentance = "leetcode";
-// let dict = ["leet", "code"];
+let sentance1 = "leetcode";
+let dict1 = ["leet", "code"];
 // true
-// let sentance = "catsandog";
-// let dict = ["cats", "dog", "sand", "and", "cat"];
+let sentance2 = "catsandog";
+let dict2 = ["cats", "dog", "sand", "and", "cat"];
 // false
 
-let sentance = "aaaaaaa";
-let dict = ["aaaa", "aaa"];
+let sentance3 = "aaaaaaa";
+let dict3 = ["aaaa", "aaa"];
 // true
-console.log(wordBreak(sentance, dict));
+console.log(wordBreak(sentance3, dict3));
